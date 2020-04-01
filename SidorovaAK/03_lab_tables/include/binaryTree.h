0eb217fcd209739aa4b821bf62e47cbe860d2008
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2020-04-01 12:36:04 +0300

    add binaryTree declaration

--- SidorovaAK/03_lab_tables/include/binaryTree.h
+++ SidorovaAK/03_lab_tables/include/binaryTree.h
@@ -1,10 +1,28 @@
 #ifndef _BINARYTREE_H_
 #define _BINARYTREE_H_
 
-class binaryTree
+#include "tnode.h"
+
+template<typename TKey, class TData>
+class BinaryTree
 {
+private:
+	TNode<TKey, TData>* root;
+
 public:
+	BinaryTreee();
+	BinaryTree(TNode<TKey, TData>* root);
+	BinaryTreee(const BinaryTree<TKey, TData>& _copy);
+	~BinaryTree();
 
+	TNode<TKey, TData>* SearchKey(const TKey _key) const;
+	TNode<TKey, TData>* SearchMaxKey() const;
+	TNode<TKey, TData>* SearchMinKey() const;
+	TNode<TKey, TData>* SearchNext(const TKey _key) const;
+	TNode<TKey, TData>* SearchPrev(const TKey _key) const;
+	
+	void Insert(const TKey _key, const TData _pData);
+	void Remove(const TKey _key);
 };
-
+//----------------------------------------------------
 #endif
