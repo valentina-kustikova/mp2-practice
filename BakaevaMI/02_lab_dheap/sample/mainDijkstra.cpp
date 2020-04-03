28deb99728b7885cb18fa3b6719a4eb2e0f46196
Maria Bakaeva <mary396969@gmail.com>
2020-04-03 17:58:54 +0300

    Algorithms work

--- BakaevaMI/02_lab_dheap/sample/mainDijkstra.cpp
+++ BakaevaMI/02_lab_dheap/sample/mainDijkstra.cpp
@@ -6,45 +6,41 @@ using namespace std;
 
 void main()
 {
-    Graph gr;
-    int start;
-    vector<vector<int> > paths;
+	cout << "\t -Dijkstra's Algorithm-" << endl;
+	Graph gr;
+	int start;
+	vector<vector<int> > paths;
 
-    try
-    {
-        cin >> gr;
-    }
-    catch (Exception_errors & e)
-    {
-        cerr << e.what() << endl;
-    }
+	try
+	{
+		cin >> gr;
 
-    do
-    {
-        cout << " Enter starting vertex:";
-        cin >> start;
-    } while ((start < 0) && (start > gr.GetSize()));
+		do
+		{
+			cout << "Enter starting vertex: ";
+			cin >> start;
+			cout << endl;
+		} while ((start < 0) || (start > gr.GetSize()));
 
-    float* res_dist = new float[gr.GetSize()];
-    memset(res_dist, 0, sizeof(float) * gr.GetSize());
+		float* res_dist = new float[gr.GetSize()];
+		memset(res_dist, 0, sizeof(float) * gr.GetSize());
 
-    Dijkstra::DijkstraAlg(gr, start, res_dist, paths);
+		Dijkstra::DijkstraAlg(gr, start, res_dist, paths);
 
-    for (int i = 0; i < gr.GetSize(); i++)
-    {
-        cout << " " << res_dist[i];
-    }
-    cout << endl;
+		for (int i = 0; i < gr.GetSize(); i++)
+		{
+			cout << " Vertex " << i << " path ";
 
-    for (int i = 0; i < gr.GetSize(); i++)
-    {
-        cout << " Vertex " << i << " path ";
+			for (int j = 0; j < paths[i].size(); j++)
+			{
+				cout << " " << paths[i][j];
+			}
 
-        for (int j = 0; j < paths[i].size(); j++)
-        {
-            cout << " " << paths[i][j];
-        }
-
-        cout << "\t W = " << res_dist[i] << endl;
-    };
+			cout << "\t W = " << res_dist[i] << endl;
+		};
+	}
+	catch (Exception_errors & e)
+	{
+		cerr << e.what() << endl;
+	}
 }
--- BakaevaMI/02_lab_dheap/src/Dijkstra.cpp
+++ BakaevaMI/02_lab_dheap/src/Dijkstra.cpp
@@ -22,33 +22,20 @@ void Dijkstra::DijkstraAlg(const Graph& gr, int start, float* res_dist, vector<v
 	TDHeap<Pair> heapVisited(gr.GetSize(), 2, dist, gr.GetSize());
 	heapVisited.Hilling();
 
-	/*//Создание списка смежности
-	vector<vector<int>> list_sm(gr.GetSize());
-	for (int i = 0; i < gr.GetCountEdges(); i++)
-	{
-		list_sm[gr.GetEdges()[i].GetBegin()].push_back(gr.GetEdges()[i].GetEnd());
-		list_sm[gr.GetEdges()[i].GetEnd()].push_back(gr.GetEdges()[i].GetBegin());
-	};*/
-
 	float* w = gr.GetWeightMatrix();
 
-	for (int i = 0; i < heapVisited.GetSize(); i++)
+	while(heapVisited.GetSize() != 0)
 	{
 		Pair min = heapVisited.GetElements()[0];
-		cout << endl << "dist[0].first  " << dist[0].first << endl;
 		heapVisited.DeleteMin();
-		cout << endl << "dist[0].first  " << dist[0].first << endl;
 
 		for(int j = 0; j < gr.GetSize(); j++)
 		{
-			cout << endl << "dist[j].first  " << dist[j].first << endl;
-			cout << "w[] = " << w[min.first * gr.GetSize() + dist[j].first] << endl;
-
 			if (((min.second +
-				w[min.first * gr.GetSize() + dist[j].first]) < dist[j].second) && (w[j] >= 0))
+				w[min.first * gr.GetSize() + dist[j].first]) < dist[j].second) && 
+				(w[min.first * gr.GetSize() + dist[j].first] > 0))
 			{
-				dist[j].second = min.second +
-					w[min.first * gr.GetSize() + dist[j].first];
+				dist[j].second = min.second + w[min.first * gr.GetSize() + dist[j].first];
 
 				visited[dist[j].first] = min.first;
 
@@ -56,14 +43,6 @@ void Dijkstra::DijkstraAlg(const Graph& gr, int start, float* res_dist, vector<v
 				w[min.first + dist[j].first * gr.GetSize()] = -1;
 			}
 		}
-
-		//Вывод dist
-		cout << endl;
-		for (int i = 0; i < gr.GetSize(); i++)
-		{
-			cout << "  " << dist[i].second; //Метки
-		}
-		cout << endl;
 
 		heapVisited.Hilling();
 	}
