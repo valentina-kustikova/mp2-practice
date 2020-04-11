99abdefcea47f4c03b2ce5bc99b6840125af15c7
vla5924 <vla2000@inbox.ru>
2020-04-11 21:45:02 +0300

    exc catch

--- VlasovMS/02_lab_dheap/sample/dijkstra.cpp
+++ VlasovMS/02_lab_dheap/sample/dijkstra.cpp
@@ -3,46 +3,53 @@
 
 int main()
 {
-    std::cout << "Test Graph 2:\n";
-    TGraph graph2({
-        TEdge(0, 1, 5),
-        TEdge(1, 2, 2),
-        TEdge(0, 2, 8),
-        TEdge(1, 4, 5),
-        TEdge(2, 4, 2),
-        TEdge(0, 3, 11),
-        TEdge(2, 3, 3),
-        TEdge(3, 5, 8),
-        TEdge(4, 5, 6),
-        TEdge(4, 7, 1),
-        TEdge(5, 6, 13),
-        TEdge(5, 7, 24),
-        TEdge(6, 7, 5)
-        }, 8);
-    std::cout << graph2;
-    std::cout << "Paths Data 2:\n";
-    TPathData pathData = GraphAlgorithms::dijkstraAlgorithm(graph2);
-    TPathList pathList = pathData.getPaths();
-    for (size_t i = 0; i < pathList.size(); i++)
+    try
     {
-        std::cout << '[' << i << "] dist: " << pathData.getDist(TVertexId(i)) << " path: ";
-        for (size_t j = 0; j < pathList[i].size(); j++)
-            std::cout << pathList[i][j] << ' ';
-        std::cout << '\n';
-    }
+        std::cout << "Test Graph 1:\n";
+        TGraph graph1({
+            TEdge(0, 1, 5),
+            TEdge(1, 2, 2),
+            TEdge(0, 2, 8),
+            TEdge(1, 4, 5),
+            TEdge(2, 4, 2),
+            TEdge(0, 3, 11),
+            TEdge(2, 3, 3),
+            TEdge(3, 5, 8),
+            TEdge(4, 5, 6),
+            TEdge(4, 7, 1),
+            TEdge(5, 6, 13),
+            TEdge(5, 7, 24),
+            TEdge(6, 7, 5)
+            }, 8);
+        std::cout << graph1;
+        std::cout << "Paths Data 1:\n";
+        TPathData pathData = GraphAlgorithms::dijkstraAlgorithm(graph1);
+        TPathList pathList = pathData.getPaths();
+        for (size_t i = 0; i < pathList.size(); i++)
+        {
+            std::cout << '[' << i << "] dist: " << pathData.getDist(TVertexId(i)) << " path: ";
+            for (size_t j = 0; j < pathList[i].size(); j++)
+                std::cout << pathList[i][j] << ' ';
+            std::cout << '\n';
+        }
 
-    TGraph graph3;
-    std::cout << "Test Graph 3:\nInput >> ";
-    std::cin >> graph3;
-    std::cout << graph3;
-    std::cout << "Paths Data 3:\n";
-    pathData = GraphAlgorithms::dijkstraAlgorithm(graph3);
-    pathList = pathData.getPaths();
-    for (size_t i = 0; i < pathList.size(); i++)
+        TGraph graph2;
+        std::cout << "Test Graph 2:\nInput >> ";
+        std::cin >> graph2;
+        std::cout << graph2;
+        std::cout << "Paths Data 2:\n";
+        pathData = GraphAlgorithms::dijkstraAlgorithm(graph2);
+        pathList = pathData.getPaths();
+        for (size_t i = 0; i < pathList.size(); i++)
+        {
+            std::cout << '[' << i << "] dist: " << pathData.getDist(TVertexId(i)) << " path: ";
+            for (size_t j = 0; j < pathList[i].size(); j++)
+                std::cout << pathList[i][j] << ' ';
+            std::cout << '\n';
+        }
+    }
+    catch (std::exception & e)
     {
-        std::cout << '[' << i << "] dist: " << pathData.getDist(TVertexId(i)) << " path: ";
-        for (size_t j = 0; j < pathList[i].size(); j++)
-            std::cout << pathList[i][j] << ' ';
-        std::cout << '\n';
+        std::cerr << e.what();
     }
 }
--- VlasovMS/02_lab_dheap/sample/kruskal.cpp
+++ VlasovMS/02_lab_dheap/sample/kruskal.cpp
@@ -3,25 +3,32 @@
 
 int main()
 {
-    TGraph graph1({
-        TEdge(1, 4, 0),
-        TEdge(1, 3, 1),
-        TEdge(2, 4, 2),
-        TEdge(0, 5, 2),
-        TEdge(1, 5, 2),
-        TEdge(0, 4, 3),
-        TEdge(3, 4, 4),
-        TEdge(2, 3, 5),
-        TEdge(0, 1, 8),
-        TEdge(2, 5, 9),
-        TEdge(1, 2, 10)
-        }, 6);
-    std::cout << "Test Graph 1:\n" << graph1;
-    std::cout << "Paths Tree 1:\n" << GraphAlgorithms::kruskalAlgorithm(graph1);
+    try
+    {
+        TGraph graph1({
+            TEdge(1, 4, 0),
+            TEdge(1, 3, 1),
+            TEdge(2, 4, 2),
+            TEdge(0, 5, 2),
+            TEdge(1, 5, 2),
+            TEdge(0, 4, 3),
+            TEdge(3, 4, 4),
+            TEdge(2, 3, 5),
+            TEdge(0, 1, 8),
+            TEdge(2, 5, 9),
+            TEdge(1, 2, 10)
+            }, 6);
+        std::cout << "Test Graph 1:\n" << graph1;
+        std::cout << "Paths Tree 1:\n" << GraphAlgorithms::kruskalAlgorithm(graph1);
 
-    TGraph graph3;
-    std::cout << "Test Graph 3:\nInput >> ";
-    std::cin >> graph3;
-    std::cout << graph3;
-    std::cout << "Paths Tree 3:\n" << GraphAlgorithms::kruskalAlgorithm(graph3);
+        TGraph graph2;
+        std::cout << "Test Graph 2:\nInput >> ";
+        std::cin >> graph2;
+        std::cout << graph2;
+        std::cout << "Paths Tree 2:\n" << GraphAlgorithms::kruskalAlgorithm(graph2);
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << e.what();
+    }
 }
