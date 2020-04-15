76a5715422ca1ca7a17efe46454afa36e0e8d49a
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2020-04-15 17:32:48 +0300

    template balancetree

---
+++ SidorovaAK/03_lab_tables/include/BalanceTree.h
@@ -0,0 +1,14 @@
+#ifndef _BALANCETREE_H_
+#define _BALANCETREE_H_
+
+#include "binarytree.h"
+#include "tbalancenode.h"
+
+class BalanceTree : public BinaryTree
+{
+public:
+
+private:
+
+};
+#endif
