c27eecf5493cea2114334c8a4074559da9dd2f52
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2020-04-30 21:27:40 +0300

    Update TList.h

--- include/TList.h
+++ include/TList.h
@@ -277,7 +277,7 @@ void TList<TData, TKey>::Remove(TKey fkey)
         pPrev = fpPrev;
         pCur = fpCur;
         pNext = fpNext;
-		throw fkey;///////////// throw key "The list doesn't contain an elem with this key - "
+	throw "The list doesn't contain an elem with this key";
     }
 	
 	if (pFirst == pCur)
