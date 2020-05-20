d89e374196325810cd22034a980bb662dcb9b753
Alexandr-Solovev <43129356+Alexandr-Solovev@users.noreply.github.com>
2020-05-20 21:53:16 +0300

    hm

--- SolovevAY/02_lab_dheap/sample/DijkstraAlgorithm/Source.cpp
+++ SolovevAY/02_lab_dheap/sample/DijkstraAlgorithm/Source.cpp
@@ -3,17 +3,7 @@
 void main() {
 	try {
 		Graph graph(1);
-		int vertex;
-		int* dist;
-		int* path;
-
-		cin >> graph;
-		cout << "Enter start algorithm vertex: ";
-		cin >> vertex;
-
-		DijkstraAlgorithm::dijkstraAlgorithm(graph, dist, path, vertex);
-		for (int i = 0; i < graph.getVertexCount(); i++)
-			cout << "Vertex: " << i << " Distance: " << dist[i] << endl;
+		] << endl;
 	}
 	catch (const exception & e) {
 		cout << e.what() << endl;
