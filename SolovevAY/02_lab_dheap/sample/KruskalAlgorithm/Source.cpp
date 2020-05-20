3b578055a0d51ee1866e5a3b2161797746eb563b
Alexandr-Solovev <43129356+Alexandr-Solovev@users.noreply.github.com>
2020-05-20 21:54:04 +0300

    hm

--- SolovevAY/02_lab_dheap/sample/KruskalAlgorithm/Source.cpp
+++ SolovevAY/02_lab_dheap/sample/KruskalAlgorithm/Source.cpp
@@ -1,17 +1,5 @@
 #include "KruskalAlgorithm.h"
 
 void main() {
-	try {
-
-		Graph graph(1);
-
-		cin >> graph;
-
-		cout << KruskalAlgorithm::kruskalAlgorithm(graph);
-
-	}
-	catch (const exception & e) {
-		cout << e.what() << endl;
-	}
-	system("pause");
+	
 }
