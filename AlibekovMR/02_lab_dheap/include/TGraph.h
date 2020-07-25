50aa5a4b01f43048eec0b3e564bc8e1df28fdd80
AlibekovMurad5202 <MuradAlibekov2000@gmail.com>
2020-07-26 00:35:06 +0300

    TGraph.h is done

--- AlibekovMR/02_lab_dheap/include/TGraph.h
+++ AlibekovMR/02_lab_dheap/include/TGraph.h
@@ -13,22 +13,25 @@ public:
 
   explicit TGraph(int _verticesCount);
   TGraph(int _verticesCount, int _edgesCount, TWeightedEdge* _edges);
-  TGraph(int _verticesCount, double* AdjacencyMatrix);
+  TGraph(int _verticesCount, double* _adjacencyMatrix);
   TGraph(const TGraph& _graph);
   ~TGraph();
 
-  bool isConnected() const;
+  bool isConnected()    const;
   bool isDisconnected() const;
-  bool hasLoop() const;
+  bool isDirected()     const;
+  bool isUndirected()   const;
+  bool hasLoop()        const;
 
   int numberOfComponents() const;
 
   double* getAdjacencyMatrix() const;
-  void printAdjacencyMatrix() const;
+  void printAdjacencyMatrix()  const;
 
-  TGraph generateRandomGraph(int _verticesCount, int _min, int _max);
-  TGraph generateRandomConnectedGraph(int _verticesCount, int _min, int _max);
-  TGraph generateRandomConnectedGraphWithoutLoops(int _verticesCount, int _min, int _max);
+  static TGraph generateRandomDigraph(int _verticesCount, int _min, int _max);
+  static TGraph generateRandomUndirectedGraph(int _verticesCount, int _min, int _max);
+  static TGraph generateRandomConnectedGraph(int _verticesCount, int _min, int _max);
+  static TGraph generateRandomConnectedGraphWithoutLoops(int _verticesCount, int _min, int _max);
 
   friend std::ostream& operator<<(std::ostream& out, const TGraph& _graph);
   friend std::istream& operator>>(std::istream& in, TGraph& _graph);
