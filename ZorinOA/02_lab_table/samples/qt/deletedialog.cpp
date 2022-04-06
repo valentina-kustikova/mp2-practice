1a75538ff6c83281aad0dff363bbdab205f90170
oleg-w570 <ol3g.zor1n@yandex.ru>
2022-04-06 19:05:59 +0300

    add qt app

---
+++ ZorinOA/02_lab_table/samples/qt/deletedialog.cpp
@@ -0,0 +1,20 @@
+#include "deletedialog.h"
+#include "ui_deletedialog.h"
+
+DeleteDialog::DeleteDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::DeleteDialog)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::SubWindow);
+}
+
+DeleteDialog::~DeleteDialog()
+{
+    delete ui;
+}
+
+QString DeleteDialog::key() const
+{
+    return ui->lineKey->text();
+}
---
+++ ZorinOA/02_lab_table/samples/qt/mainwindow.cpp
@@ -0,0 +1,277 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::updateHash()
+{
+    std::stringstream tmp;
+    tmp << H;
+    ui->tableHash->setText(QString::fromStdString(tmp.str()));
+}
+
+
+void MainWindow::updateSort()
+{
+    std::stringstream tmp;
+    tmp << S;
+    ui->tableSort->setText(QString::fromStdString(tmp.str()));
+}
+
+
+void MainWindow::updateScan()
+{
+    std::stringstream tmp;
+    tmp << T;
+    ui->tableScan->setText(QString::fromStdString(tmp.str()));
+}
+
+void MainWindow::updatePolyStr()
+{
+    poly1_str = ui->line1Poly->text().toStdString();
+    poly2_str = ui->line2Poly->text().toStdString();
+}
+
+void MainWindow::updateRes(const Polinom& poly)
+{
+    std::stringstream tmp;
+    tmp << poly;
+    res_str = tmp.str();
+    ui->lineResPoly->setText(QString::fromStdString(res_str));
+}
+
+
+void MainWindow::on_pushButton_SUB_clicked()
+{
+    updatePolyStr();
+    Polinom res = Polinom(poly1_str) - Polinom(poly2_str);
+    updateRes(res);
+}
+
+
+void MainWindow::on_pushButton_ADD_clicked()
+{
+    try {
+        updatePolyStr();
+        Polinom res = Polinom(poly1_str) + Polinom(poly2_str);
+        updateRes(res);
+    }  catch (std::string& err) {
+        std::cout << err << std::endl;
+    }
+}
+
+
+void MainWindow::on_pushButton_MULT_clicked()
+{
+    updatePolyStr();
+    Polinom res = Polinom(poly1_str) * Polinom(poly2_str);
+    updateRes(res);
+}
+
+
+void MainWindow::on_lineKey_textChanged(const QString &arg1)
+{
+    key = arg1.toStdString();
+}
+
+
+void MainWindow::on_pushButton_toHash_clicked()
+{
+    H.Insert(key, Polinom(res_str));
+    updateHash();
+}
+
+
+void MainWindow::on_pushButton_toSort_clicked()
+{
+    S.Insert(key, Polinom(res_str));
+    updateSort();
+}
+
+
+void MainWindow::on_pushButton_toScan_clicked()
+{
+    T.Insert(key, Polinom(res_str));
+    updateScan();
+}
+
+
+void MainWindow::on_pushButton_Hash_add_clicked()
+{
+    InputDialog* pID = new InputDialog;
+    if (pID->exec() == QDialog::Accepted)
+    {
+        H.Insert(pID->key().toStdString(), pID->poly().toStdString());
+        updateHash();
+    }
+    delete pID;
+}
+
+
+void MainWindow::on_pushButton_Hash_del_clicked()
+{
+    DeleteDialog* pDD = new DeleteDialog;
+    if (pDD->exec() == QDialog::Accepted)
+    {
+        H.Delete(pDD->key().toStdString());
+        updateHash();
+    }
+    delete pDD;
+}
+
+
+void MainWindow::on_pushButton_Hash_clear_clicked()
+{
+    H.Clear();
+    updateHash();
+}
+
+
+void MainWindow::on_pushButton_Sort_add_clicked()
+{
+    InputDialog* pID = new InputDialog;
+    if (pID->exec() == QDialog::Accepted)
+    {
+        S.Insert(pID->key().toStdString(), pID->poly().toStdString());
+        updateSort();
+    }
+    delete pID;
+}
+
+
+void MainWindow::on_pushButton_Sort_del_clicked()
+{
+    DeleteDialog* pDD = new DeleteDialog;
+    if (pDD->exec() == QDialog::Accepted)
+    {
+        S.Delete(pDD->key().toStdString());
+        updateSort();
+    }
+    delete pDD;
+}
+
+
+void MainWindow::on_pushButton_Sort_clear_clicked()
+{
+    S.Clear();
+    updateSort();
+}
+
+
+void MainWindow::on_pushButton_Scan_add_clicked()
+{
+    InputDialog* pID = new InputDialog;
+    if (pID->exec() == QDialog::Accepted)
+    {
+        T.Insert(pID->key().toStdString(), pID->poly().toStdString());
+        updateScan();
+    }
+    delete pID;
+}
+
+
+void MainWindow::on_pushButton_Scan_del_clicked()
+{
+    DeleteDialog* pDD = new DeleteDialog;
+    if (pDD->exec() == QDialog::Accepted)
+    {
+        T.Delete(pDD->key().toStdString());
+        updateScan();
+    }
+    delete pDD;
+}
+
+
+void MainWindow::on_pushButton_Scan_clear_clicked()
+{
+    T.Clear();
+    updateScan();
+}
+
+
+void MainWindow::on_pushButton_toAll_clicked()
+{
+    H.Insert(key, Polinom(res_str));
+    S.Insert(key, Polinom(res_str));
+    T.Insert(key, Polinom(res_str));
+    updateHash();
+    updateSort();
+    updateScan();
+}
+
+
+void MainWindow::on_select1Poly_clicked()
+{
+    SelectDialog* pSD = new SelectDialog;
+    if (pSD->exec() == QDialog::Accepted)
+    {
+        std::string tmp_key = pSD->key().toStdString();
+        int table_choise = pSD->table_choise();
+        Polinom* tmp_poly;
+        if (table_choise == 1)
+            tmp_poly = H.Find(tmp_key);
+        else if (table_choise == 2)
+            tmp_poly = S.Find(tmp_key);
+        else
+            tmp_poly = T.Find(tmp_key);
+
+        if (tmp_poly != nullptr)
+        {
+            std::stringstream tmp;
+            tmp << *tmp_poly;
+            poly1_str = tmp.str();
+            ui->line1Poly->setText(QString::fromStdString(poly1_str));
+        }
+    }
+    delete pSD;
+}
+
+
+void MainWindow::on_select2Poly_clicked()
+{
+    SelectDialog* pSD = new SelectDialog;
+    if (pSD->exec() == QDialog::Accepted)
+    {
+        std::string tmp_key = pSD->key().toStdString();
+        int table_choise = pSD->table_choise();
+        Polinom* tmp_poly;
+        if (table_choise == 1)
+            tmp_poly = H.Find(tmp_key);
+        else if (table_choise == 2)
+            tmp_poly = S.Find(tmp_key);
+        else
+            tmp_poly = T.Find(tmp_key);
+
+        if (tmp_poly != nullptr)
+        {
+            std::stringstream tmp;
+            tmp << *tmp_poly;
+            poly2_str = tmp.str();
+            ui->line2Poly->setText(QString::fromStdString(poly2_str));
+        }
+    }
+    delete pSD;
+}
+
+
+void MainWindow::on_pushButton_calculate_clicked()
+{
+    poly1_str = ui->line1Poly->text().toStdString();
+    double x = ui->lineX->text().toDouble();
+    double y = ui->lineY->text().toDouble();
+    double z = ui->lineZ->text().toDouble();
+    double value = Polinom(poly1_str)(x, y, z);
+    ui->lineValue->setText(QString::number(value));
+}
+
