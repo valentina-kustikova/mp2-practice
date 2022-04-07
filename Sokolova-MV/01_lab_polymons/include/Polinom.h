9402182cb4fce47749f8f67ba3de8975e5366cd7
Maria02179 <73477221+Maria02179@users.noreply.github.com>
2022-04-07 17:12:51 +0300

    Update Polinom.h

--- Sokolova-MV/01_lab_polymons/include/Polinom.h
+++ Sokolova-MV/01_lab_polymons/include/Polinom.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -6,44 +7,31 @@
 #include "Monom.h"
 
 
-class TPolinom : public THeadList<TMonom>
-{
+class TPolinom : public THeadList<TMonom> {
 public:
 
-	TPolinom()
-	{
+	TPolinom() {
 		TMonom m;
 		pHead->data = m;
 	}
-	TPolinom(TMonom& m)
-	{
+
+	TPolinom(TMonom& m) {
 		insCurrent(m);
 	}
-	TPolinom(const TPolinom& p)
-	{
+
+	TPolinom(const TPolinom& p) {
 
 		auto pCurr = p.pFirst;
 		auto pPrev = p.pStop;
-		
-		for (; !(pCurr == p.pStop); )
-		{
+
+		for (; !(pCurr == p.pStop);) {
 			insLast(pCurr->data);
 			pPrev = pCurr;
 			pCurr = pCurr->pNext;
 		}
-	}/**/
-	/*TPolinom(TPolinom& p)
-	{
-		for (p.reset(); !(p.isEnd()); p.goNext())
-		{
-			insLast(p.pCurr->data);
-		}
-	}/**/
+	}
 
-
-
-	void addMonom(TMonom& m)
-	{
+	void addMonom(TMonom& m) {
 		{
 			for (reset(); !isEnd(); goNext()) {
 				if (m == pCurr->data) {
@@ -63,14 +51,12 @@ public:
 			}
 		}
 	}
-	TPolinom operator*(const int& c)
-	{
+
+	TPolinom operator*(const int& c) {
 		TPolinom res(*this);
 		res.reset();
-		while (!res.isEnd())
-		{
-			if (c != 0)
-			{
+		while (!res.isEnd()) {
+			if (c != 0) {
 				res.pCurr->data.coeff *= c;
 				res.goNext();
 			}
@@ -80,14 +66,12 @@ public:
 		}
 		return res;
 	}
-	TPolinom operator*(const TMonom& m)
-	{
+
+	TPolinom operator*(const TMonom& m) {
 		TPolinom res(*this);
 		res.reset();
-		while (!res.isEnd())
-		{
-			if (m.coeff != 0)
-			{
+		while (!res.isEnd()) {
+			if (m.coeff != 0) {
 				int x, y, z;
 				x = res.pCurr->data.degree / 100;
 				y = (res.pCurr->data.degree - x * 100) / 10;
@@ -96,9 +80,8 @@ public:
 				x += m.degree / 100;
 				y += (m.degree / 10) % 10;
 				z += m.degree % 10;
-				if (x >= 10 || y >= 10 || z >= 10)
-				{
-					throw("Выход за границу степени");
+				if (x >= 10 || y >= 10 || z >= 10) {
+					throw ("Выход за границу степени");
 					break;
 				}
 				res.pCurr->data.degree = x * 100 + y * 10 + z;
@@ -110,80 +93,77 @@ public:
 		}
 		return res;
 	}
-	TPolinom operator*(TPolinom& p)
-	{
+
+	TPolinom operator*(TPolinom& p) {
 		TPolinom tmp1(*this);
 		TPolinom res;
 		p.reset();
-		while (!p.isEnd())
-		{
+		while (!p.isEnd()) {
 			tmp1.reset();
 			res = res + tmp1 * p.pCurr->data;
 			p.goNext();
-			if (!tmp1.isEnd())
-			{
+			if (!tmp1.isEnd()) {
 				p.reset();
 			}
 		}
 		return res;
 	}
-	TPolinom& operator=(TPolinom& p)
-	{
-		p.reset();
+
+	TPolinom& operator=(const TPolinom& p) {
+		TPolinom tmp(p);
+		tmp.reset();
 		reset();
-		while (!isEnd())
-		{
+		while (!isEnd()) {
 			RemoveCurr();
 			goNext();
 		}
-		while (!p.isEnd())
-		{
-			addMonom(p.getCurrdata());
-			p.goNext();
+		while (!tmp.isEnd()) {
+			TMonom t = tmp.getCurrdata();
+			addMonom(t);
+			tmp.goNext();
 		}
 		return *this;
 	}
-	bool operator==(const TPolinom& p) const 
-	{
+
+	bool operator==(const TPolinom& p) const {
 		TPolinom res(p);
 		TPolinom pthis(*this);
 		res.reset();
 		pthis.reset();
-		while (!res.isEnd())
-		{
-			if ((pthis.getCurrdata()==res.getCurrdata())==0) return false;
+		while (!res.isEnd()) {
+			if ((pthis.getCurrdata() == res.getCurrdata()) == 0) return false;
 			res.goNext();
 			pthis.goNext();
 		}
 		return true;
 	}
-	TPolinom operator-(TMonom& p) { return (*this + p * (-1.0)); }
-	TPolinom operator-(TPolinom& p) { return (*this + p * (-1.0)); }
-	TPolinom operator+(const TPolinom& p) const
-	{
+
+	TPolinom operator-(TMonom& p) {
+		TMonom tmp = p * (-1);
+		return (*this + tmp);
+	}
+
+	TPolinom operator-(TPolinom& p) { return (*this + p * (-1)); }
+
+	TPolinom operator+(const TPolinom& p) const {
 		TPolinom pthis(*this);
 		TPolinom res(p);
 		res.reset();
 		pthis.reset();
-		while (!pthis.isEnd())
-		{
+		while (!pthis.isEnd()) {
 			if (res.pCurr->data.degree > pthis.pCurr->data.degree)
 				res.goNext();
-			else if (res.pCurr->data.degree < pthis.pCurr->data.degree)
-			{
+			else if (res.pCurr->data.degree < pthis.pCurr->data.degree) {
 				res.insCurrent(pthis.pCurr->data);
 				pthis.goNext();
 			}
-			else
-			{
+			else {
 				res.pCurr->data.coeff += pthis.pCurr->data.coeff;
-				if (res.pCurr->data.coeff != 0)
-				{
+				if (res.pCurr->data.coeff != 0) {
 					res.goNext();
 					pthis.goNext();
 				}
-				else
-				{
+				else {
 					res.RemoveCurr();
 					pthis.goNext();
 				}
@@ -191,105 +171,96 @@ public:
 		}
 		return res;
 	}
-	TPolinom operator+(TMonom& m)
-	{
+
+	TPolinom operator+(TMonom& m) {
 		TPolinom res;
 		res.insCurrent(m);
 		res = *this + res;
 		return res;
 	}
-	friend ostream& operator<<(ostream& os, TPolinom& p)
-	{
+
+	friend ostream& operator<<(ostream& os, TPolinom& p) {
 		for (p.reset(); !p.isEnd(); p.goNext()) {
 			os << p.getCurrdata();
-			if (p.pCurr->pNext != p.pStop)
-			{
+			if (p.pCurr->pNext != p.pStop) {
 				os << "+ ";
 			}
 		}
 		os << "\n";
 		return os;
 	}
-	friend istream& operator>>(istream& in, TPolinom& P)
-	{
+
+	friend istream& operator>>(istream& in, TPolinom& P) {
 		string str;
 		in >> str;
 		P = TPolinom(str);
 		return in;
 	}
-	TPolinom(string str)
-	{
+
+	TPolinom(string str) {
 		string p = str;
 		int i = 0;
 
-		while (i < str.length())
-		{
-			if (str[i] == ' ' || str[i] == '+') { i++; continue; }
+		while (i < str.length()) {
+			if (str[i] == ' ' || str[i] == '+') {
+				i++;
+				continue;
+			}
 			string coeff;
-			for (; isdigit(str[i]) || str[i] == ' ' || str[i] == ',' || str[i] == '-'; i++)
-			{
+			for (; isdigit(str[i]) || str[i] == ' ' || str[i] == ',' || str[i] == '-'; i++) {
 				if (str[i] == ' ') continue;
 				coeff.push_back(str[i]);
 			}
 			string degX;
-			if (str[i] == 'x')
-			{
+			if (str[i] == 'x') {
 				i += 1;
-				if (str[i] != '^')
-				{
+				if (str[i] != '^') {
 					degX = "1";
 				}
 				else {
 					i += 1;
-					while (isdigit(str[i]))
-					{
+					while (isdigit(str[i])) {
 						degX.push_back(str[i]);
 						i++;
 					}
 				}
 			}
 			string degY;
-			if (str[i] == 'y')
-			{
+			if (str[i] == 'y') {
 				i += 1;
-				if (str[i] != '^')
-				{
+				if (str[i] != '^') {
 					degY = "1";
 				}
 				else {
 					i += 1;
-					while (isdigit(str[i]))
-					{
+					while (isdigit(str[i])) {
 						degY.push_back(str[i]);
 						i++;
 					}
 				}
 			}
 			string degZ;
-			if (str[i] == 'z')
-			{
+			if (str[i] == 'z') {
 				i += 1;
-				if (str[i] != '^')
-				{
+				if (str[i] != '^') {
 					degZ = "1";
 				}
 				else {
 					i += 1;
-					while (isdigit(str[i]))
-					{
+					while (isdigit(str[i])) {
 						degZ.push_back(str[i]);
 						i++;
 					}
 				}
 			}
-			double _coeff = 1; int _degX = 0, _degY = 0, _degZ = 0;
+			double _coeff = 1;
+			int _degX = 0, _degY = 0, _degZ = 0;
 			if (!coeff.empty()) _coeff = stod(coeff);
 			if (!degX.empty()) _degX = stoi(degX);
 			if (!degY.empty()) _degY = stoi(degY);
 			if (!degZ.empty()) _degZ = stoi(degZ);
-			if ((_degX > 9) || (_degY > 9) || (_degZ > 9))
-			{
-				throw("Выход за границу степени");
+			if ((_degX > 9) || (_degY > 9) || (_degZ > 9)) {
+				throw ("Выход за границу степени");
 				break;
 			}
 			int d = _degX * 100 + _degY * 10 + _degZ;
@@ -298,6 +269,5 @@ public:
 		}
 	}
 
-	
-};
 
+};
