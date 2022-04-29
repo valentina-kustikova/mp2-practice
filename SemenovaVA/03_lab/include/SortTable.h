292d857a94881bea164f15b4c5c70c9b76da01bf
nika-semen <nika-semen@mail.ru>
2022-04-29 10:17:57 +0300

    Attempt№1

---
+++ SemenovaVA/03_lab/include/SortTable.h
@@ -0,0 +1,153 @@
+#include "Table.h"
+
+//Класс упорядоченные таблицы
+template <typename TData, typename TKey>
+class SortTable : public ScanTable<TData, TKey>
+{
+protected:
+	void SortData(); // метод сортировки записей по ключам
+public:
+	// Конструкторы, деструктор	
+	SortTable(unsigned int n = 10) : ScanTable<TData, TKey>(n) {};
+	SortTable(const SortTable<TData, TKey>& T1);
+	SortTable(const ScanTable<TData, TKey>& T1);
+	~SortTable() {};
+	// Методы 
+	virtual void Insert(const TData data, const TKey key);
+	virtual TData* Search(const TKey key);
+	virtual void Delete(const TKey key);
+};
+
+// Pелизация функциий для класса упорядоченных таблиц
+// Конструктор
+template <typename TData, typename TKey>
+SortTable<TData, TKey>::SortTable(const SortTable<TData, TKey>& T1)
+{
+	Size = T1.Size;
+	ind = T1.ind;
+	dataCount = T1.dataCount;
+	rec = new TabRecord<TData, TKey>*[Size];
+	for (int i = 0; i < dataCount; i++)
+	{
+		rec[i] = new TabRecord<TData, TKey>(*(T1.rec[i]));
+	}
+}
+template <typename TData, typename TKey>
+SortTable<TData, TKey>::SortTable(const ScanTable<TData, TKey>& T1)
+{
+	Size = T1.Size;
+	dataCount = T1.dataCount;
+	ind = T1.ind;
+	rec = new TabRecord<TData, TKey>*[Size];
+	for (int i = 0; i < dataCount; i++)
+	{
+		rec[i] = T1.rec[i];
+	}
+	SortData();
+}
+// Методы 
+template <typename TData, typename TKey>
+void SortTable<TData, TKey>::Insert(const TData data, const TKey key)
+{
+	if (IsFull()) { throw 1; } //Таблица переполнена
+	TabRecord<TData, TKey> R(key, data);
+	Reset();
+	while (!(IsEnd()) && key >= rec[ind]->GetKey())
+	{
+		if (rec[ind]->GetKey() == key)
+		{
+			throw - 1;
+		} //Ключ уже есть в таблице
+		ind++;
+	}
+	if (IsEmpty())
+	{
+		ind++;
+	}
+	dataCount++;
+	for (int i = dataCount - 1; i > ind; i--)
+	{
+		rec[i] = rec[i - 1];
+	}
+	rec[ind] = new TabRecord<TData, TKey>(R);
+	Reset();
+}
+template <typename TData, typename TKey>
+TData* SortTable<TData, TKey>::Search(const TKey key)
+{
+	Reset();
+	TData* tmp = nullptr;
+	if (!(IsEmpty()))
+	{
+		int i = -1, j = dataCount;
+		int mid;
+		while (i < j - 1)
+		{
+			mid = (j + i) / 2;
+			if (key >= rec[mid]->GetKey())
+			{
+				i = mid;
+			}
+			else
+			{
+				j = mid;
+			}
+		}
+		if (key == rec[i]->GetKey())
+		{
+			ind = i;
+			tmp = rec[ind]->GetData();
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+	else
+	{
+		throw 1;
+	}
+	return tmp;
+}
+template <typename TData, typename TKey>
+void SortTable<TData, TKey>::Delete(const TKey key)
+{
+	Reset();
+	if (Search(key) == nullptr)
+	{
+		throw 1;
+	} // элемента для удаления нет в таблице
+	else
+	{
+		if (dataCount > 1)
+		{
+			dataCount--;
+			for (int i = ind; i < dataCount; i++)
+			{
+				rec[i] = rec[i + 1];
+			}
+			Reset();
+		}
+		else
+		{
+			dataCount = 0;
+		}
+	}
+}
+template <typename TData, typename TKey>
+void SortTable<TData, TKey>::SortData()
+{
+	int i, j;
+	for (i = 0; i < dataCount; i++)
+	{
+		for (j = 0; j < dataCount; j++)
+		{
+			if (rec[i]->GetKey() < rec[j]->GetKey())
+			{
+				TabRecord<TData, TKey>* temp(rec[i]);
+				rec[i] = rec[j];
+				rec[j] = temp;
+			}
+		}
+	}
+}
---
+++ SemenovaVA/03_lab/include/Table.h
@@ -0,0 +1,120 @@
+#pragma once 
+#include <iostream>
+using namespace std;
+
+//Класс Запись
+template <typename TData, typename TKey>
+class TabRecord
+{
+protected:
+	TKey key;
+	TData* data;
+public:
+	// Конструкторы, деструктор	
+	TabRecord(TKey k = {}, TData d = {}) { key = k; data = new TData(d); }
+	TabRecord(const TabRecord& T1) { key = T1.key; data = new TData(*(T1.data)); } // конструктор копирования
+	~TabRecord() { delete data; }
+	//Методы
+	TData* GetData() const { return data; }
+	TKey GetKey() const { return key; }
+	// Перегрузка операций
+	TabRecord<TData, TKey>& operator=(const TabRecord<TData, TKey>& TabR)
+	{
+		data = new TData(*(TabR.data)); key = TabR.key; return *this;
+	}
+};
+
+//Класс Таблица
+template <typename TData, typename TKey>
+class Table
+{
+protected:
+	TabRecord<TData, TKey>** rec;
+	unsigned int Size;
+	unsigned int dataCount;
+	int ind;
+public:
+	// Конструкторы, деструктор	
+	Table(unsigned int n = 10);
+	virtual ~Table() { delete[] rec; }
+	// Методы 
+	virtual void Insert(const TData Data, const TKey Key) = 0;
+	virtual void Delete(const TKey Key) = 0;
+	virtual TData* Search(const TKey Key) = 0;
+	void Reset();
+	void SetNext();
+	bool IsEnd() const { return ind == dataCount || ind == -1; }
+	bool IsFull() const { return dataCount == Size; }
+	bool IsEmpty() const { return dataCount == 0; }
+	int GetCount() const { return dataCount; }
+	int GetSize() const { return Size; }
+	TData* GetData() const; // для текущего элемента
+	TKey GetKey() const;  // для текущего элемента
+	template<class TData> friend std::ostream& operator<< (std::ostream& os, const Table<TData, TKey>& Tab)
+	{
+		unsigned int i = 0;
+		while (i < Tab.dataCount)
+		{
+			os << "\t" << left << setw(15) << Tab.rec[i]->GetKey() << " | " << *(Tab.rec[i]->GetData()) << '\n';
+			i++;
+		}
+		if (Tab.dataCount == 0)
+			os << "\n\tTable is Empty\n";
+		return os;
+	}
+};
+
+// Pелизация функциий для класса таблиц
+// Конструкторы, деструктор	
+template <typename TData, typename TKey>
+Table<TData, TKey>::Table(unsigned int n)
+{
+	Size = n;
+	dataCount = 0;
+	ind = -1;
+	rec = new TabRecord<TData, TKey>*[Size];
+}
+// Методы 
+template <typename TData, typename TKey>
+void Table<TData, TKey>::Reset()
+{
+	if (dataCount > 0)
+	{
+		ind = 0;
+	}
+	else
+	{
+		ind = -1;
+	}
+}
+template <typename TData, typename TKey>
+void Table<TData, TKey>::SetNext()
+{
+	if (ind != -1) { ind++; }
+	else { throw - 1; } //Таблица пуста
+	if (IsEnd()) { Reset(); }
+}
+template <typename TData, typename TKey>
+TData* Table<TData, TKey>::GetData() const
+{
+	if (!IsEmpty())
+	{
+		return rec[ind]->GetData();
+	}
+	else
+	{
+		throw - 1;
+	} //Таблица пуста
+}
+template <typename TData, typename TKey>
+TKey Table<TData, TKey>::GetKey() const
+{
+	if (!IsEmpty())
+	{
+		return rec[ind]->GetKey();
+	}
+	else
+	{
+		throw - 1;
+	} //Таблица пуста
+}
