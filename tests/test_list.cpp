ce1e5ebe7cd316e3b57b30f414efb1b061537d4f
Виктория Суслова <73444769+vikkkkos@users.noreply.github.com>
2022-05-12 21:48:00 +0300

    n

--- tests/test_list.cpp
+++ tests/test_list.cpp
@@ -1,17 +1,38 @@
 #include <gtest.h>
 #include "list.h"
+#include "headlist.h"
+
+TEST(Node, can_create_new_node)
+{
+    ASSERT_NO_THROW(Node<int> n);
+}
 
 TEST(List, can_create_empty_list)
 {
     ASSERT_NO_THROW(List<int> m);
 }
-
 TEST(List, list_is_empty)
 {
     List<double> list;
     EXPECT_EQ(true, list.isEmpty());
 }
-
+TEST(List, can_copy_empty_list)
+{
+    List<int> l;
+    ASSERT_NO_THROW(List<int> m(l));
+}
+TEST(List, copy_list_is_empty)
+{
+    List<int> l;
+    List<int> m(l);
+    EXPECT_EQ(true, l.isEmpty());
+}
+TEST(List, list_is_not_empty)
+{
+    List<int> l;
+    l.InsertToTail(4);
+    EXPECT_EQ(false, l.isEmpty());
+}
 TEST(List, list_is_ended)
 {
     List<double> list;
@@ -19,14 +40,27 @@ TEST(List, list_is_ended)
     list.InsertToHead(2);
     EXPECT_EQ(true, list.isEnd());
 }
-
+TEST(List, can_insert_to_head0)
+{
+    List<int> l;
+    ASSERT_NO_THROW(l.InsertToHead(1));
+}
+TEST(List, can_insert_to_tail0)
+{
+    List<int> l;
+    ASSERT_NO_THROW(l.InsertToTail(1));
+}
+TEST(List, can_insert_to_order0)
+{
+    List<int> l;
+    ASSERT_NO_THROW(l.InsertInOrder(1));
+}
 TEST(List, can_insert_to_head)
 {
     List<int> list;
     list.InsertToHead(1);
     EXPECT_EQ(1, list.Head());
 }
-
 TEST(List, can_insert_to_head2)
 {
     List<int> list;
@@ -34,14 +68,12 @@ TEST(List, can_insert_to_head2)
     list.InsertToHead(2);
     EXPECT_EQ(2, list.Head());
 }
-
 TEST(List, can_insert_to_tail)
 {
     List<int> list;
     list.InsertToTail(1);
     EXPECT_EQ(1, list.Head());
 }
-
 TEST(List, can_insert_to_tail2)
 {
     List<int> list;
@@ -49,7 +81,6 @@ TEST(List, can_insert_to_tail2)
     list.InsertToTail(2);
     EXPECT_EQ(1, list.Head());
 }
-
 TEST(List, can_clean)
 {
     List<int> list;
@@ -57,7 +88,6 @@ TEST(List, can_clean)
     list.Clean();
     EXPECT_EQ(true, list.isEmpty());
 }
-
 TEST(List, can_clean2)
 {
     List<int> list;
@@ -67,7 +97,6 @@ TEST(List, can_clean2)
     list.Clean();
     EXPECT_EQ(true, list.isEmpty());
 }
-
 TEST(List, can_delete)
 {
     List<int> list;
@@ -78,7 +107,6 @@ TEST(List, can_delete)
     Node<int>* tmp = list.GetHead();
     EXPECT_EQ(2, tmp->data);
 }
-
 TEST(List, can_delete2)
 {
     List<int> list;
@@ -90,3 +118,18 @@ TEST(List, can_delete2)
     tmp = tmp->next;
     EXPECT_EQ(3, tmp->data);
 }
+TEST(HeadList, can_create_headlist)
+{
+    ASSERT_NO_THROW(HeadList<int> m);
+}
+TEST(HeadList, can_insert)
+{
+    HeadList<int> h;
+    ASSERT_NO_THROW(h.InsertToTail(1));
+}
+TEST(HeadList, can_delete)
+{
+    HeadList<int> h;
+    ASSERT_NO_THROW(h.DeleteFirst());
+}
+
--- tests/test_polinom.cpp
+++ tests/test_polinom.cpp
@@ -1,170 +1,176 @@
 #include <gtest.h>
-#include "polinom.h"
+#include "polynom.h"
 
 //тесты из методички------------------------------------------------
-TEST(Polinom, from_book_1)
+TEST(Polynom, from_book_1)
 {
-    Polinom P("3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z");
-    Polinom Q("4x^3y^2z^6-6x^2yz^8");
-    Polinom S("3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z+4x^3y^2z^6-6x^2yz^8");
+    Polynom P("3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z");
+    Polynom Q("4x^3y^2z^6-6x^2yz^8");
+    Polynom S("3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z+4x^3y^2z^6-6x^2yz^8");
     EXPECT_EQ(S, P + Q);
 }
 
-TEST(Polinom, from_book_2)
+TEST(Polynom, from_book_2)
 {
-    Polinom P("3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z");
-    Polinom Q("4x^7y^2z^6-6x^6yz^8");
-    Polinom S("4x^7y^2z^6-6x^6yz^8+3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z");
+    Polynom P("3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z");
+    Polynom Q("4x^7y^2z^6-6x^6yz^8");
+    Polynom S("4x^7y^2z^6-6x^6yz^8+3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z");
     EXPECT_EQ(S, P + Q);
 }
 
-TEST(Polinom, from_book_3)
+TEST(Polynom, from_book_3)
 {
-    Polinom P("3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z");
-    Polinom Q("4x^5y^2z^5+5x^4y^3z^3");
-    Polinom S("7x^5y^2z^5+7x^3y^5z");
+    Polynom P("3x^5y^2z^5-5x^4y^3z^3+7x^3y^5z");
+    Polynom Q("4x^5y^2z^5+5x^4y^3z^3");
+    Polynom S("7x^5y^2z^5+7x^3y^5z");
     EXPECT_EQ(S, P + Q);
 }
 
-TEST(Polinom, from_book_4)
+TEST(Polynom, from_book_4)
 {
-    Polinom P("3x^5y^2z^5-5x^4y^3z^3+7x^7y^5z");
-    Polinom Q("4x^6y^2z^6-6x^2yz^8");
-    Polinom S("7x^7y^5z+4x^6y^2z^6+3x^5y^2z^5-5x^4y^3z^3-6x^2yz^8");
+    Polynom P("3x^5y^2z^5-5x^4y^3z^3+7x^7y^5z");
+    Polynom Q("4x^6y^2z^6-6x^2yz^8");
+    Polynom S("7x^7y^5z+4x^6y^2z^6+3x^5y^2z^5-5x^4y^3z^3-6x^2yz^8");
     EXPECT_EQ(S, P + Q);
 }
 
-TEST(Polinom, from_book_5)
+TEST(Polynom, from_book_5)
 {
-    Polinom P("3x^5y^2z^5-5x^4y^3z^3+7x^7y^5z");
-    Polinom Q("-3x^5y^2z^5+5x^4y^3z^3-7x^7y^5z");
-    Polinom S("0");
+    Polynom P("3x^5y^2z^5-5x^4y^3z^3+7x^7y^5z");
+    Polynom Q("-3x^5y^2z^5+5x^4y^3z^3-7x^7y^5z");
+    Polynom S("0");
     EXPECT_EQ(S, P + Q);
 }
 //----------------------------------------------------------------------
 
-TEST(Polinom, can_create_polinom)
+TEST(Polynom, can_create_polynom)
 {
-    ASSERT_NO_THROW(Polinom p);
+    ASSERT_NO_THROW(Polynom p);
 }
 
-TEST(Polinom, can_create_polinom_from_string)
+TEST(Polynom, can_create_polynom_from_string)
 {
     string s = "x+y+2z";
-    ASSERT_NO_THROW(Polinom p(s));
+    ASSERT_NO_THROW(Polynom p(s));
 }
 
-TEST(Polinom, build_polinom)
+TEST(Polynom, can_copy_polynom)
 {
-    ASSERT_NO_THROW(Polinom p("x+1"));
+    Polynom p;
+    ASSERT_NO_THROW(Polynom m(p));
 }
 
-TEST(Polinom, degree_no_more_than_9_1)
+TEST(Polynom, build_polynom)
 {
-    Polinom p("x^5");
-    Polinom q("x^4");
+    ASSERT_NO_THROW(Polynom p("x+1"));
+}
+
+TEST(Polynom, degree_no_more_than_9_1)
+{
+    Polynom p("x^5");
+    Polynom q("x^4");
     ASSERT_NO_THROW(p * q);
 }
 
-TEST(Polinom, degree_no_more_than_9_2)
+TEST(Polynom, degree_no_more_than_9_2)
 {
-    Polinom p("x^5");
-    Polinom q("x^5");
+    Polynom p("x^5");
+    Polynom q("x^5");
     ASSERT_ANY_THROW(p * q);
 }
 
-TEST(Polinom, addition_1)
+TEST(Polynom, addition_1)
 {
-    Polinom p("x+y");
-    Polinom q("x+y");
-    Polinom s("2x+2y");
+    Polynom p("x+y");
+    Polynom q("x+y");
+    Polynom s("2x+2y");
     EXPECT_EQ(s, p + q);
 }
 
-TEST(Polinom, addition_2)
+TEST(Polynom, addition_2)
 {
-    Polinom p("3x^2");
-    Polinom q("4x^7");
-    Polinom s("3x^2+4x^7");
+    Polynom p("3x^2");
+    Polynom q("4x^7");
+    Polynom s("3x^2+4x^7");
     EXPECT_EQ(s, p + q);
 }
 
-TEST(Polinom, addition_3)
+TEST(Polynom, addition_3)
 {
-    Polinom p("x+y+z");
-    Polinom q("x-2y+z^2");
-    Polinom s("2x-y+z+z^2");
+    Polynom p("x+y+z");
+    Polynom q("x-2y+z^2");
+    Polynom s("2x-y+z+z^2");
     EXPECT_EQ(s, p + q);
 }
 
-TEST(Polinom, subtraction_1)
+TEST(Polynom, subtraction_1)
 {
-    Polinom p("x^3+5");
-    Polinom q("x^2-4");
-    Polinom s("-x^2+x^3+9");
+    Polynom p("x^3+5");
+    Polynom q("x^2-4");
+    Polynom s("-x^2+x^3+9");
     EXPECT_EQ(s, p - q);
 }
 
-TEST(Polinom, subtraction_2)
+TEST(Polynom, subtraction_2)
 {
-    Polinom p("x+y+z");
-    Polinom q("y");
-    Polinom s("x+z");
+    Polynom p("x+y+z");
+    Polynom q("y");
+    Polynom s("x+z");
     EXPECT_EQ(s, p - q);
 }
 
-TEST(Polinom, subtraction_3)
+TEST(Polynom, subtraction_3)
 {
-    Polinom p("x+2y");
-    Polinom q("x-z");
-    Polinom s("z+2y");
+    Polynom p("x+2y");
+    Polynom q("x-z");
+    Polynom s("z+2y");
     EXPECT_EQ(s, p - q);
 }
 
-TEST(Polinom, polinomial_multiplication_1)
+TEST(Polynom, polynomial_multiplication_1)
 {
-    Polinom p("x+1");
-    Polinom q("x-1");
-    Polinom s("x^2-1");
+    Polynom p("x+1");
+    Polynom q("x-1");
+    Polynom s("x^2-1");
     EXPECT_EQ(s, p * q);
 }
 
-TEST(Polinom, polinomial_multiplication_2)
+TEST(Polynom, polynomial_multiplication_2)
 {
-    Polinom p("x^3");
-    Polinom q("y^4");
-    Polinom s("x^3y^4");
+    Polynom p("x^3");
+    Polynom q("y^4");
+    Polynom s("x^3y^4");
     EXPECT_EQ(s, p * q);
 }
 
-TEST(Polinom, polinomial_multiplication_3)
+TEST(Polynom, polynomial_multiplication_3)
 {
-    Polinom p("15x^2");
-    Polinom q("10x^2+14y^2");
-    Polinom s("150x^4+210x^2y^2");
+    Polynom p("15x^2");
+    Polynom q("10x^2+14y^2");
+    Polynom s("150x^4+210x^2y^2");
     EXPECT_EQ(s, p * q);
 }
 
-TEST(Polinom, multiplication_polinom_and_const_1)
+TEST(Polynom, multiplication_polynom_and_const_1)
 {
-    Polinom p("x^2");
+    Polynom p("x^2");
     double d = 10.0;
-    Polinom s("10.0x^2");
+    Polynom s("10.0x^2");
     EXPECT_EQ(s, p * d);
 }
 
-TEST(Polinom, multiplication_polinom_and_const_2)
+TEST(Polynom, multiplication_polynom_and_const_2)
 {
-    Polinom p("x^2+y^2");
+    Polynom p("x^2+y^2");
     double d = 10.0;
-    Polinom s("10.0x^2+10.0y^2");
+    Polynom s("10.0x^2+10.0y^2");
     EXPECT_EQ(s, p * d);
 }
 
-TEST(Polinom, multiplication_polinom_and_const_3)
+TEST(Polynom, multiplication_polynom_and_const_3)
 {
-    Polinom p("2x^2+y-z");
+    Polynom p("2x^2+y-z");
     double d = 5.5;
-    Polinom s("11x^2+5.5y-5.5z");
+    Polynom s("11x^2+5.5y-5.5z");
     EXPECT_EQ(s, p * d);
 }
