580ea8bca91fdcaf3648e85ee4e887fb664ddf2c
nika-semen <74305462+nika-semen@users.noreply.github.com>
2022-05-14 10:11:42 +0300

    Attempt №5

--- SemenovaVA/03_lab/include/AVLTree.h
+++ SemenovaVA/03_lab/include/AVLTree.h
@@ -4,72 +4,72 @@
 using namespace std;
 #define PTBalanceNode TbNode<TData,TKey>*
 
-//класс Звено АВЛ дерева
+//РєР»Р°СЃСЃ Р—РІРµРЅРѕ РђР’Р› РґРµСЂРµРІР°
 template <typename TData, typename TKey>
 class  TbNode : public TrNode<TData, TKey> 
-{ protected:
-  int balance; // индекс балансировки вершины (-1,0,1)
-  public:
-  // Конструкторы
+{ public:
+  int balance; // РёРЅРґРµРєСЃ Р±Р°Р»Р°РЅСЃРёСЂРѕРІРєРё РІРµСЂС€РёРЅС‹ (-1,0,1)
+  // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂС‹
   TbNode(TKey k = {}, TData d = {}, TrNode* P = nullptr, TrNode* R = nullptr, TrNode* L = nullptr, int bal = 0): TrNode<TData, TKey> (k, d, P, L, R), balance(bal) {};
-  //Методы
-  int GetBalance() const { return balance; }
-  void SetBalance(int bal) { balance = bal; }
  };
 
-//Класс АВЛ дерево
+//РљР»Р°СЃСЃ РђР’Р› РґРµСЂРµРІРѕ
 template <typename TData, typename TKey>
 class  AVLTree : public BSTree<TData, TKey>
 { protected:
-  //Методы
+  //РњРµС‚РѕРґС‹
   int Insert(PTBalanceNode& N1, TKey k, TData d);
   int Delete(PTBalanceNode& N1, TKey k);
   int LeftBal(PTBalanceNode& N1);                        
   int RightBal(PTBalanceNode& N1);                       
   public:
-  // Конструкторы
+  // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂС‹
   AVLTree() :BSTree<TData, TKey>() {}                                       
-  //Методы
-  void Insert(TKey k, TData d);                  
+  //РњРµС‚РѕРґС‹
+  void Insert(TKey k, TData d);   //РІРёСЂС‚СѓР°Р»СЊРЅС‹Рµ               
   void Delete(TKey k);
 };
 
-// Pелизация функциий для класса АВЛ дерево
+// PРµР»РёР·Р°С†РёСЏ С„СѓРЅРєС†РёРёР№ РґР»СЏ РєР»Р°СЃСЃР° РђР’Р› РґРµСЂРµРІРѕ
 template <typename TData, typename TKey>
 void AVLTree<TData, TKey>::Insert(TKey k, TData d) 
-{
-   if(Find(k)!=nullptr)
-  { throw 1; }
+{ 
+if(Find(k)!=nullptr) { throw 1; } //РїРѕРІС‚РѕСЂРЅР°СЏ РІРІСЃС‚Р°РІРєР° СЌР»РµРјРµРЅС‚Р° 
+   
+
   Insert((PTBalanceNode&)Root, k, d); 
+
 }
 template <typename TData, typename TKey>
 int AVLTree<TData, TKey>::Insert(PTBalanceNode& N1, TKey k, TData d) 
-{ if (N1 == nullptr) 
+{ if (N1 == nullptr) // РІСЃС‚Р°РІРєР° РєРѕСЂРЅСЏ
   { N1= new TbNode<TData, TKey>(k, d);
     return 1; 
-  }
+  } 
   if (k < N1->GetKey()) 
-  { if (N1->Left == nullptr) // вставка вершины
+  { if (N1->Left == nullptr) // РІСЃС‚Р°РІРєР° РІРµСЂС€РёРЅС‹
     { N1->Left = new TbNode<TData, TKey>(k, d, N1);
       return LeftBal(N1);
     }
-    if (Insert((PTBalanceNode&)(N1->Left), k, d) == 1) // после вставки высота левого поддерева увеличилась - балансировка
+    if (Insert((PTBalanceNode&)(N1->Left), k, d) == 1) // РїРѕСЃР»Рµ РІСЃС‚Р°РІРєРё РІС‹СЃРѕС‚Р° Р»РµРІРѕРіРѕ РїРѕРґРґРµСЂРµРІР° СѓРІРµР»РёС‡РёР»Р°СЃСЊ - Р±Р°Р»Р°РЅСЃРёСЂРѕРІРєР°
     { return LeftBal(N1);}
   }
   if (k > N1->GetKey())
-  { if (N1->Right == nullptr)   // вставка вершины
+  { if (N1->Right == nullptr)   // РІСЃС‚Р°РІРєР° РІРµСЂС€РёРЅС‹
     { N1->Right = new TbNode<TData, TKey>(k, d, N1);
     return RightBal(N1);           
     }
-    if (Insert((PTBalanceNode&)(N1->Right), k, d) == 1) // после вставки высота правого поддерева увеличилась - балансировка
+    if (Insert((PTBalanceNode&)(N1->Right), k, d) == 1) // РїРѕСЃР»Рµ РІСЃС‚Р°РІРєРё РІС‹СЃРѕС‚Р° РїСЂР°РІРѕРіРѕ РїРѕРґРґРµСЂРµРІР° СѓРІРµР»РёС‡РёР»Р°СЃСЊ - Р±Р°Р»Р°РЅСЃРёСЂРѕРІРєР°
     { return RightBal(N1); }
   }
 } 
 template <typename TData, typename TKey>
 void AVLTree<TData, TKey>::Delete(TKey k)
-{ if(Find(k)==nullptr)
+{ 
+if(Find(k)==nullptr)
   {throw 1;}
   Delete((PTBalanceNode&)Root, k); 
+
 }
 template <typename TData, typename TKey>
 int AVLTree<TData, TKey>::Delete(PTBalanceNode& N1, TKey k)
@@ -95,35 +95,35 @@ int AVLTree<TData, TKey>::Delete(PTBalanceNode& N1, TKey k)
 }
 template <typename TData, typename TKey>
 int AVLTree<TData, TKey>::LeftBal(PTBalanceNode& N1) 
-{ switch (N1->GetBalance()) 
-  { case 1: N1->SetBalance(0);   // в поддереве был перевес справа
-           break;               // устанавливается равновесие
-    case 0: N1->SetBalance(-1);  // в поддереве было равновесие
-            break;               // устанавливается перевес слева
-    case -1:                     // в поддереве был перевес слева - необходима балансировка
+{ switch (N1->balance) 
+  { case 1: N1->balance=0;   // РІ РїРѕРґРґРµСЂРµРІРµ Р±С‹Р» РїРµСЂРµРІРµСЃ СЃРїСЂР°РІР°
+           break;                // СѓСЃС‚Р°РЅР°РІР»РёРІР°РµС‚СЃСЏ СЂР°РІРЅРѕРІРµСЃРёРµ
+    case 0: N1->balance=-1;  // РІ РїРѕРґРґРµСЂРµРІРµ Р±С‹Р»Рѕ СЂР°РІРЅРѕРІРµСЃРёРµ
+            break;               // СѓСЃС‚Р°РЅР°РІР»РёРІР°РµС‚СЃСЏ РїРµСЂРµРІРµСЃ СЃР»РµРІР°
+    case -1:                     // РІ РїРѕРґРґРµСЂРµРІРµ Р±С‹Р» РїРµСЂРµРІРµСЃ СЃР»РµРІР° - РЅРµРѕР±С…РѕРґРёРјР° Р±Р°Р»Р°РЅСЃРёСЂРѕРІРєР°
            PTBalanceNode p1;
            PTBalanceNode p2;
            PTBalanceNode p3;
            p1 = (PTBalanceNode)(N1->Left);
-           p3 = (PTBalanceNode)(*N1).Parent;
-           if (p1->GetBalance() == -1)   // случай 1 - однократный LL поворот
+           p3 = (PTBalanceNode)(N1->Parent);
+           if (p1->balance == -1)   // СЃР»СѓС‡Р°Р№ 1 - РѕРґРЅРѕРєСЂР°С‚РЅС‹Р№ LL РїРѕРІРѕСЂРѕС‚
            { //1
              if (p1->Right!= nullptr)
-             { (*(p1->Right)).Parent = N1; }
+             { p1->Right->Parent = N1; }
              N1->Left = p1->Right;  
              //2
              if (N1 != nullptr)
-             { (*(N1)).Parent = p1; }
+             { N1->Parent = p1; }
              p1->Right = N1;       
-             N1->SetBalance(0); 
+             N1->balance=0;
              (*p1).Parent = p3;
              N1 = p1;
            }
-           else  // случай 2 - двукратный LR поворот
+           else  // СЃР»СѓС‡Р°Р№ 2 - РґРІСѓРєСЂР°С‚РЅС‹Р№ LR РїРѕРІРѕСЂРѕС‚
            { p2 = (PTBalanceNode)(p1->Right);
              //1
              if (p2->Left != nullptr)
-             { (*(p2->Left)).Parent = p1; }
+             { p2->Left->Parent = p1; }
              p1->Right = p2->Left;
              //2
              if (p1 != nullptr) 
@@ -131,74 +131,74 @@ int AVLTree<TData, TKey>::LeftBal(PTBalanceNode& N1)
              p2->Left = p1;
              //3
              if (p2->Right != nullptr)
-             { (*(p2->Right)).Parent = N1; }
+             { p2->Right->Parent = N1; }
              N1->Left = p2->Right;
              //4
              if (N1 != nullptr) 
              { (*N1).Parent = p2; }
              p2->Right = N1;
-             if (p2->GetBalance() == -1) N1->SetBalance(1);
-             else N1->SetBalance(0);  
-             if (p2->GetBalance() == 1) N1->SetBalance(-1);
-             else p1->SetBalance(0);     
+             if (p2->balance == -1) N1->balance=1;
+             else N1->balance = 0;
+             if (p2->balance == 1) N1->balance=-1;
+             else p1->balance = 0;
              (*p2).Parent= p3;
              N1 = p2;
            }
-           N1->SetBalance(0); 
+           N1->balance=0;
    }
-  return abs(N1->GetBalance());
+  return abs(N1->balance);
 }       
 template <typename TData, typename TKey>
 int AVLTree<TData, TKey>::RightBal(PTBalanceNode& N1) 
-{ switch (N1->GetBalance())
-  { case -1: N1->SetBalance(0);        // в поддереве был перевес слева
-             break;                    // устанавливается равновесие
-    case 0:  N1->SetBalance(1);        // в поддереве было равновесие
-             break;                    // устанавливается перевес справа
-    case 1:  // в поддереве был перевес справа - необходима балансировка
+{ switch (N1->balance)
+  { case -1: N1->balance=0;        // РІ РїРѕРґРґРµСЂРµРІРµ Р±С‹Р» РїРµСЂРµРІРµСЃ СЃР»РµРІР°
+             break;                    // СѓСЃС‚Р°РЅР°РІР»РёРІР°РµС‚СЃСЏ СЂР°РІРЅРѕРІРµСЃРёРµ
+    case 0:  N1->balance=1;        // РІ РїРѕРґРґРµСЂРµРІРµ Р±С‹Р»Рѕ СЂР°РІРЅРѕРІРµСЃРёРµ
+             break;                    // СѓСЃС‚Р°РЅР°РІР»РёРІР°РµС‚СЃСЏ РїРµСЂРµРІРµСЃ СЃРїСЂР°РІР°
+    case 1:  // РІ РїРѕРґРґРµСЂРµРІРµ Р±С‹Р» РїРµСЂРµРІРµСЃ СЃРїСЂР°РІР° - РЅРµРѕР±С…РѕРґРёРјР° Р±Р°Р»Р°РЅСЃРёСЂРѕРІРєР°
              PTBalanceNode p1;
              PTBalanceNode p2;
-             PTBalanceNode p3 = (PTBalanceNode)(*N1).Parent;
+             PTBalanceNode p3 = (PTBalanceNode)N1->Parent;
              p1 = (PTBalanceNode)(N1->Right);
-             if (p1->GetBalance() == 1) // случай 1 - однократный RR поворот
+             if (p1->balance == 1) // СЃР»СѓС‡Р°Р№ 1 - РѕРґРЅРѕРєСЂР°С‚РЅС‹Р№ RR РїРѕРІРѕСЂРѕС‚
              { //1
                if (p1->Left != nullptr)
-               { (*p1->Left).Parent = N1; }
+               { p1->Left->Parent = N1; }
                N1->Right = p1->Left; 
                //2
                if (N1 != nullptr)
                { (*N1).Parent = p1; }
                p1->Left = N1;
-               N1->SetBalance(0);   
+               N1->balance=0;
                N1 = p1;
                (*p1).Parent = p3;
              }
-             else // случай 2 - двукратный RL поворот
+             else // СЃР»СѓС‡Р°Р№ 2 - РґРІСѓРєСЂР°С‚РЅС‹Р№ RL РїРѕРІРѕСЂРѕС‚
              { p2 = (PTBalanceNode)(p1->Left);
                //1
                if (p2->Right != nullptr)
-               { (*(p2->Right)).Parent = p1; }
+               { p2->Right->Parent = p1; }
                p1->Left = p2->Right;    
                //2
                if (p1 != nullptr)
-               { (*(p1)).Parent = p2; }
+               { p1->Parent = p2; }
                p2->Right = p1;      
                //3
                if (p2->Left != nullptr)
-               { (*p2->Left).Parent = N1; }
+               { p2->Left->Parent = N1; }
                N1->Right = p2->Left;      
                //4
                if (N1 != nullptr)
                { (*N1).Parent = p2; }
                p2->Left = N1;   
-               if (p2->GetBalance() == 1) N1->SetBalance(-1);
-               else N1->SetBalance(0);  
-               if (p2->GetBalance() == -1) N1->SetBalance(1);
-               else p1->SetBalance(0);     
+               if (p2->balance == 1) N1->balance=-1;
+               else N1->balance = 0;
+               if (p2->balance == -1) N1->balance=1;
+               else p1->balance = 0;
                (*p2).Parent = p3;
                N1 = p2;
              }
-             N1->SetBalance(0); 
+             N1->balance=0;
   }
-  return abs(N1->GetBalance());;
+  return abs(N1->balance);;
 }
