4f960b3d94164925cc278903d98d3626ac137643
Artem Shipilov <hugo.nieto@mail.ru>
2022-05-16 09:06:37 +0300

    changes added

--- ShipilovAY/01_lab_polymons/include/node.h
+++ ShipilovAY/01_lab_polymons/include/node.h
@@ -4,7 +4,9 @@ class TNode {
 public:
 	T data;
 	TNode<T>* pNext;
-	TNode() {};
+	TNode() {
+		pNext = nullptr;
+	};
 	~TNode() {};
 	TNode(const T _data, TNode<T>* Node = nullptr) { data = _data; pNext = Node; }
 	TNode<T>& operator=(const TNode<T>& _TNode)
@@ -13,8 +15,12 @@ public:
 		pNext = _TNode.pNext;
 		return *this;
 	};
-	bool operator == (const TNode<T>& Node) const
+	bool operator==(const TNode<T>& Node) const
 	{
 		return *this->pNext == Node.pNext;
+	};
+	bool operator!=(const TNode<T>& Node) const
+	{
+		return !(*this == Node);
 	};
 };
