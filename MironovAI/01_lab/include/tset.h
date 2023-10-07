c91718a14e666d9e33fedb9a424450e545baa84c
Fest1veNapkin <arseniy.mironov.01@mail.ru>
2023-10-07 15:58:18 +0400

    First commit

---
+++ MironovAI/01_lab/include/tset.h
@@ -0,0 +1,34 @@
+#ifndef __SET_H__
+#define __SET_H__
+
+#include "tbitfield.h"
+
+class TSet
+{
+private:
+  int MaxPower;       // максимальная мощность множества
+  TBitField BitField; // битовое поле для хранения характеристического вектора
+public:
+  TSet(int mp);
+  TSet(const TSet &s);       // конструктор копирования
+  TSet(const TBitField &bf); // конструктор преобразования типа
+  operator TBitField();      // преобразование типа к битовому полю
+  // доступ к битам
+  int GetMaxPower(void) const;     // максимальная мощность множества
+  void InsElem(const int Elem);       // включить элемент в множество
+  void DelElem(const int Elem);       // удалить элемент из множества
+  int IsMember(const int Elem) const; // проверить наличие элемента в множестве
+  // теоретико-множественные операции
+  int operator== (const TSet &s) const; // сравнение
+  int operator!= (const TSet &s) const; // сравнение
+  const TSet& operator=(const TSet &s);  // присваивание
+  TSet operator+ (const int Elem); // объединение с элементом                                  
+  TSet operator- (const int Elem); // разность с элементом                                  
+  TSet operator+ (const TSet &s);  // объединение
+  TSet operator* (const TSet &s);  // пересечение
+  TSet operator~ (void);           // дополнение
+
+  friend istream &operator>>(istream &istr, TSet &bf);
+  friend ostream &operator<<(ostream &ostr, const TSet &bf);
+};
+#endif
