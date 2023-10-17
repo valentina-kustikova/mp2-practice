34377e4fda0e74c3f30661e73e76d37bbf40b2b6
itabunov <vanatabunov@gmail.com>
2023-10-17 16:04:24 +0300

    01_lab

--- TabunovID/01_lab/samples/sample_tbitfield.cpp
+++ TabunovID/01_lab/samples/sample_tbitfield.cpp
@@ -1,8 +1,31 @@
-#include <iostream>
+#include "tbitfield.h"
+
+void test_tbitfield()
+{
+	TBitField a(5);
+	a.SetBit(1);
+	a.SetBit(3);
+	cout << "A: " << a << endl;
+	TBitField b(a);
+	cout << "B " << b << endl;
+	cout << "B 1 bit: " << b.GetBit(1) << endl;
+	b.ClrBit(1);
+	cout << "B 1 bit after clear: " << b.GetBit(1) << endl;
+
+	cout << "A: " << a << endl;
+	cout << "B: " << b << endl;
+	cout << "A&B " << (a & b) << endl;
+	cout << "A|B " << (a | b) << endl;
+	cout << "A==B " << (a == b) << endl;
+	cout << "~A  " << (~a) << endl;
+	cout << "A.GetLength(): " << a.GetLength() << endl;
+	cout << "OK" << endl;
+	system("pause"); //Чтобы сразу не пропадало окно
+}
+
 
 
 int main()
 {
-    std::cout << "TBitField" << std::endl;
-    return 0;
+	test_tbitfield();
 }
--- TabunovID/01_lab/samples/sample_tset.cpp
+++ TabunovID/01_lab/samples/sample_tset.cpp
@@ -1,8 +1,32 @@
-#include <iostream>
+#include "tset.h"
+
+void test_tset()
+{
+	TSet a(5);
+	a.InsElem(1);
+	a.InsElem(3);
+	cout << "A: " << a << endl;
+	TSet b(a);
+	cout << "B " << b << endl;
+	cout << "B 1 bit: " << a.IsMember(1) << endl;
+	b.DelElem(1);
+	cout << "B 1 bit after clear: " << b.IsMember(1) << endl;
+
+	cout << "A: " << a << endl;
+	cout << "B: " << b << endl;
+	cout << "A&B " << (a * b) << endl;
+	cout << "A|B " << (a + b) << endl;
+	cout << "A==B " << (a == b) << endl;
+	cout << "~A  " << (~a) << endl;
+
+	cout << "A.GetMaxPower(): " << a.GetMaxPower() << endl;
+	cout << "OK" << endl;
+	system("pause"); //Чтобы сразу не пропадало окно
+}
+
 
 
 int main()
 {
-    std::cout << "TSet" << std::endl;
-    return 0;
+	test_tset();
 }
