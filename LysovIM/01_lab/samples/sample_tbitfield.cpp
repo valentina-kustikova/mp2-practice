44403a180125324361aea08a369a180e5cf084f1
Ivan-Lysov <ivan.lysov.05@gmail.com>
2023-10-18 08:58:47 +0300

    Two commit

--- LysovIM/01_lab/samples/sample_tbitfield.cpp
+++ LysovIM/01_lab/samples/sample_tbitfield.cpp
@@ -5,22 +5,21 @@ void bitfield()
 {
 	TBitField bf1(n);
 	cin >> bf1;
-	cout << "1:" << bf1 << endl;
+	cout << " Bietfield 1:" << bf1 << endl;
 	TBitField bf2(n);
-	bf2.SetBit(1);
-	bf2.SetBit(2);
-	bf2.SetBit(4);
-	bf2.SetBit(3);
-	bf2.SetBit(5);
-	bf2.SetBit(6);
-	cout << "2:" << bf2 << endl;
-	bf2.ClrBit(1);
-	cout << "2:" << bf2 << endl;
-	cout << "1|2:" << (bf1 | bf2) << endl;
-	cout << "1&2:" << (bf1 & bf2) << endl;
-	cout << "1==2:" << (bf1 == bf2) << endl;
+	//bf2.SetBit(1);
+	//bf2.SetBit(2);
+	//bf2.SetBit(4);
+	//bf2.SetBit(3);
+	//bf2.SetBit(5);
+	//bf2.SetBit(6);
+	cout << "Bietfield 2:" << bf2 << endl;
+	cout << "Bietfield 1 or Bietfield 2" << "\t" << (bf1 | bf2) << endl;
+	cout << "Bietfield and Bietfield 2:" << "\t" << (bf1 & bf2) << endl;
+	cout << "Bietfield 1 = Bietfield 2? :" << "\t" << (bf1 == bf2) << endl;
 	cout << "~1:" << (~bf1) << endl;
-	cout << "Bitfield Length: " << bf1.GetLength() << endl;
+	cout << "Bitfield 1 Length: " << bf1.GetLength() << endl;
+	cout << "Bitfield 2 Length: " << bf2.GetLength() << endl;
 }
 
 int main()
--- LysovIM/01_lab/samples/sample_tset.cpp
+++ LysovIM/01_lab/samples/sample_tset.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
 #include "tset.h"
-const int n = 10;
+const int n = 5;
 using namespace std;
 
 void set() {
     TSet s1(n);
     TSet s2(n);
     cin >> s1;
-    cout << "1:" << s1 << endl;
-    s2.InsElem(0);
-    s2.InsElem(1);
-    s2.InsElem(2);
-    s2.InsElem(3);
-    s2.InsElem(4);
-    s2.InsElem(5);
-    s2.InsElem(6);
-    s2.InsElem(7);
-    s2.InsElem(8);
-    s2.InsElem(9);
-    cout << "2:" << s2 << endl;
-    cout << "1==2:" << (s1 == s2) << "\n" << endl;
-    cout << "+:" << (s1 + s2) << endl;
-    cout << "+(Element):" << (s1 + 0) << endl;
-    cout << "-(Element):" << (s1 - 1) << endl;
-    cout << "*:" << (s1 * s2) << endl;
-    cout << "~1:" << ~s1 << endl;
+    cout << "Set 1:" << s1 << endl;
+    cin >> s2;
+    cout << "Set 1:" << s2 << endl;
+    cout << "Set 1 == Set 2?: " <<"\t"<< (s1 == s2) << "\n" << endl;
+    cout << "Set 1 + Set 2:" << "\t" << (s1 + s2) << endl;
+    cout << "Set 1 + Element (0):" << "\t" << (s1 + 0) << endl;
+    cout << "Set 1 - Element (1):" << "\t" << (s1 - 1) << endl;
+    cout << "Set 1 * Set 2:" << "\t" << (s1 * s2) << endl;
+    cout << "~ Set 1:" << "\t" << ~s1 << endl;
 }
 
 int main()
