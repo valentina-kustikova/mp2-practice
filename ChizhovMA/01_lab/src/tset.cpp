6a0d5aa240c2d8beecc28990c1a858bd58c9899d
unknown <ITMM-230042@Q113.local>
2023-10-25 12:07:01 +0300

    Final

--- ChizhovMA/01_lab/src/tset.cpp
+++ ChizhovMA/01_lab/src/tset.cpp
@@ -154,6 +154,8 @@ istream& operator>>(istream& istr, TSet& s) // ввод
 
 ostream& operator<<(ostream &ostr, const TSet &s) // вывод
 {
-    ostr << s.BitField;
+    for (int i = 0; i < s.MaxPower; i++)
+        if (s.IsMember(i))
+            ostr << i << ' ';
     return ostr;
 }
