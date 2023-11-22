d3056e36933cb9cab1ccf057425f357db3793fab
Arseny-Korobeinikov <arsenikorob@gmail.com>
2023-11-22 12:22:06 +0300

    fix tests

--- KorobeinikovAP/02_lab/tests/test_tmatrix.cpp
+++ KorobeinikovAP/02_lab/tests/test_tmatrix.cpp
@@ -4,7 +4,7 @@
 
 TEST(Matrix, can_create_matrix_with_positive_size)
 {
-	ASSERT_NO_THROW(TMatrix<int> m(10) );
+	ASSERT_NO_THROW(TMatrix<int> m(10));
 }
 
 TEST(Matrix, throws_when_create_matrix_with_negative_size)
@@ -16,125 +16,106 @@ TEST(Matrix, throws_when_create_matrix_with_negative_size)
 TEST(Matrix, can_create_copy_of_the_matrix)
 {
 	TMatrix<int> a(2);
-	Vector<int> av(2), bv(1);
-	av[0] = 1;
-	av[1] = 2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 2;
 
-	bv[0] = 4;
-
-	a[0] = av;
-	a[1] = bv;
-	 
 	TMatrix<int> b(a);
 
-	EXPECT_EQ(4, b[1][0]);
+	EXPECT_EQ(a, b);
 }
+
+TEST(Matrix, copied_vector_has_its_own_memory)
+{
+	TMatrix<int> a(2);
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 1;
+
+	TMatrix<int> b(a);
+	b[0][0] = 2;
+
+	EXPECT_NE(a, b);
+}
+
 
 TEST(Matrix, can_assign_matrix_with_equal_size)
 {
 	TMatrix<int> a(2);
-	Vector<int> av(2), bv(1);
-	av[0] = 1;
-	av[1] = 2;
-
-	bv[0] = 4;
-
-	a[0] = av;
-	a[1] = bv;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 1;
 
 	TMatrix<int> b(2);
 	b = a;
 
-	EXPECT_EQ(4, b[1][0]);
+	EXPECT_EQ(a, b);
 }
 
 
 TEST(Matrix, can_assign_matrix_with_not_equal_size)
 {
 	TMatrix<int> a(2);
-	Vector<int> av(2), bv(1);
-	av[0] = 1;
-	av[1] = 2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 1;
 
-	bv[0] = 4;
-
-	a[0] = av;
-	a[1] = bv;
-
-	TMatrix<int> b(100);
+	TMatrix<int> b(3);
 	b = a;
 
-	EXPECT_EQ(4, b[1][0]);
+	EXPECT_EQ(a, b);
 }
 
 
 TEST(Matrix, matrix_with_equal_size_but_not_eaual)
 {
 	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 1;
-	av1[1] = 2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
-	av2[0] = 4;
-
-	a[0] = av1;
-	a[1] = av2;
 
 
 	TMatrix<int> b(2);
-	Vector<int> bv1(2), bv2(1);
-	bv1[0] = 100;
-	bv1[1] = 100;
-
-	bv2[0] = 100;
-
-	b[0] = bv1;
-	b[1] = bv2;
+	b[0][0] = 1;
+	b[0][1] = 2;
+	b[1][0] = 10;
 
 
-	EXPECT_NE(a, b);
+	ASSERT_TRUE(a != b);
 }
 
 TEST(Matrix, equal_matrix_are_equal)
 {
-	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 1;
-	av1[1] = 2;
+	TMatrix<int> a(2), b(2);
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
-	av2[0] = 4;
+	b[0][0] = 1;
+	b[0][1] = 2;
+	b[1][0] = 3;
 
-	a[0] = av1;
-	a[1] = av2;
 
-	TMatrix<int> b(2);
-	Vector<int> bv1(2), bv2(1);
-	bv1[0] = 1;
-	bv1[1] = 2;
-
-	bv2[0] = 4;
-
-	b[0] = bv1;
-	b[1] = bv2;
-
-	EXPECT_EQ(a, b);
+	ASSERT_TRUE(a == b);
 }
 
 TEST(Matrix, matrix_with_different_size_are_not_eaual)
 {
-	TMatrix<int> a(2);
-	Vector<int> av(2), bv(1);
-	av[0] = 1;
-	av[1] = 2;
+	TMatrix<int> a(2), b(3);
 
-	bv[0] = 4;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
-	a[0] = av;
-	a[1] = bv;
+	b[0][0] = 1;
+	b[0][1] = 2;
+	b[0][2] = 3;
+	b[1][0] = 4;
+	b[1][1] = 5;
+	b[2][0] = 6;
 
-	TMatrix<int> b(100);
-	
-	EXPECT_NE(a, b);
+	ASSERT_TRUE(a != b);
 }
 
 
@@ -142,41 +123,31 @@ TEST(Matrix, matrix_with_different_size_are_not_eaual)
 TEST(Matrix, can_add_matrix_with_equal_size)
 {
 	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 1;
-	av1[1] = 2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
-	av2[0] = 4;
-
-	a[0] = av1;
-	a[1] = av2;
+	
 
 	TMatrix<int> b(2);
-	Vector<int> bv1(2), bv2(1);
-	bv1[0] = 1;
-	bv1[1] = 2;
-
-	bv2[0] = 4;
-
-	b[0] = bv1;
-	b[1] = bv2;
+	b[0][0] = 1;
+	b[0][1] = 2;
+	b[1][0] = 3;
 
 	TMatrix<int> c(2);
-	c = a + b;
-	EXPECT_EQ(8, c[1][0]);
+	c[0][0] = 2;
+	c[0][1] = 4;
+	c[1][0] = 6;
+
+	EXPECT_EQ(c, a+b);
 }
 
 TEST(Matrix, throws_when_add_matrix_with_different_size)
 {
 	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 1;
-	av1[1] = 2;
-
-	av2[0] = 4;
-
-	a[0] = av1;
-	a[1] = av2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
 	TMatrix<int> b(100);
 
@@ -187,41 +158,29 @@ TEST(Matrix, throws_when_add_matrix_with_different_size)
 TEST(Matrix, can_substract_matrix_with_equal_size)
 {
 	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 1;
-	av1[1] = 2;
-
-	av2[0] = 8;
-
-	a[0] = av1;
-	a[1] = av2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
 	TMatrix<int> b(2);
-	Vector<int> bv1(2), bv2(1);
-	bv1[0] = 1;
-	bv1[1] = 2;
-
-	bv2[0] = 4;
-
-	b[0] = bv1;
-	b[1] = bv2;
+	b[0][0] = 1;
+	b[0][1] = 2;
+	b[1][0] = 3;
 
 	TMatrix<int> c(2);
-	c = a - b;
-	EXPECT_EQ(4, c[1][0]);
+	c[0][0] = 0;
+	c[0][1] = 0;
+	c[1][0] = 0;
+
+	EXPECT_EQ(c, a - b);
 }
 
 TEST(Matrix, throws_when_substract_matrix_with_different_size)
 {
 	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 1;
-	av1[1] = 2;
-
-	av2[0] = 4;
-
-	a[0] = av1;
-	a[1] = av2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
 	TMatrix<int> b(100);
 
@@ -232,41 +191,29 @@ TEST(Matrix, throws_when_substract_matrix_with_different_size)
 TEST(Matrix, can_multiply_matrix_with_equal_size)
 {
 	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 1;
-	av1[1] = 2;
-
-	av2[0] = 4;
-
-	a[0] = av1;
-	a[1] = av2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
 	TMatrix<int> b(2);
-	Vector<int> bv1(2), bv2(1);
-	bv1[0] = 1;
-	bv1[1] = 2;
-
-	bv2[0] = 4;
-
-	b[0] = bv1;
-	b[1] = bv2;
+	b[0][0] = 1;
+	b[0][1] = 2;
+	b[1][0] = 3;
 
 	TMatrix<int> c(2);
-	c = a * b;
-	EXPECT_EQ(16, c[1][0]);
+	c[0][0] = 1;
+	c[0][1] = 8;
+	c[1][0] = 9;
+
+	EXPECT_EQ(c, a * b);;
 }
 
 TEST(Matrix, throws_when_multiply_matrix_with_different_size)
 {
 	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 1;
-	av1[1] = 2;
-
-	av2[0] = 4;
-
-	a[0] = av1;
-	a[1] = av2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
 	TMatrix<int> b(100);
 
@@ -278,28 +225,18 @@ TEST(Matrix, throws_when_multiply_matrix_with_different_size)
 TEST(Matrix, triple_assign)
 {
 	TMatrix<int> a(2);
-	Vector<int> av1(2), av2(1);
-	av1[0] = 50;
-	av1[1] = 2;
-
-	av2[0] = 100;
-
-	a[0] = av1;
-	a[1] = av2;
+	a[0][0] = 1;
+	a[0][1] = 2;
+	a[1][0] = 3;
 
 	TMatrix<int> b(2);
-	Vector<int> bv1(2), bv2(1);
-	bv1[0] = 1;
-	bv1[1] = 2;
-
-	bv2[0] = 4;
-
-	b[0] = bv1;
-	b[1] = bv2;
+	b[0][0] = 1;
+	b[0][1] = 2;
+	b[1][0] = 3;
 
 	TMatrix<int> c(2);
 	b = a;
 	c = b;
-	EXPECT_EQ(50, c[0][0]);
+	EXPECT_EQ(c,a);
 }
 
--- KorobeinikovAP/02_lab/tests/test_tvector.cpp
+++ KorobeinikovAP/02_lab/tests/test_tvector.cpp
@@ -106,7 +106,7 @@ TEST(Vector, throws_when_too_large_index_vector_with_start_index)
 }
 
 
-TEST(Vector, can_assign_vectos_with_equal_size_without_start_index)
+TEST(Vector, can_assign_vectors_with_equal_size_without_start_index)
 {
     Vector<int> vec1(3);
     vec1[0] = 1;
@@ -116,10 +116,10 @@ TEST(Vector, can_assign_vectos_with_equal_size_without_start_index)
     Vector<int> vec2(3);
     vec2 = vec1;
 
-    EXPECT_EQ(10, vec2[2]);
+    EXPECT_EQ(vec1, vec2);
 }
 
-TEST(Vector, can_assign_vectos_with_equal_size_with_start_index)
+TEST(Vector, can_assign_vectors_with_equal_size_with_start_index)
 {
     Vector<int> vec1(3,3);
     vec1[0] = 1;
@@ -129,11 +129,11 @@ TEST(Vector, can_assign_vectos_with_equal_size_with_start_index)
     Vector<int> vec2(3);
     vec2 = vec1;
 
-    EXPECT_EQ(10, vec2[2]);
+    EXPECT_EQ(vec1, vec2);
 }
 
 
-TEST(Vector, can_assign_vectos_with_not_equal_size_without_start_index)
+TEST(Vector, can_assign_vectors_with_not_equal_size_without_start_index)
 {
     Vector<int> vec1(3);
     vec1[0] = 1;
@@ -143,10 +143,10 @@ TEST(Vector, can_assign_vectos_with_not_equal_size_without_start_index)
     Vector<int> vec2(4);
     vec2 = vec1;
 
-    EXPECT_EQ(10, vec2[2]);
+    EXPECT_EQ(vec1, vec2);
 }
 
-TEST(Vector, can_assign_vectos_with_not_equal_size_with_equal_start_index)
+TEST(Vector, can_assign_vectors_with_not_equal_size_with_equal_start_index)
 {
     Vector<int> vec1(3, 3);
     vec1[0] = 1;
@@ -156,10 +156,10 @@ TEST(Vector, can_assign_vectos_with_not_equal_size_with_equal_start_index)
     Vector<int> vec2(4, 3);
     vec2 = vec1;
 
-    EXPECT_EQ(10, vec2[2]);
+    EXPECT_EQ(vec1, vec2);
 }
 
-TEST(Vector, can_assign_vectos_with_not_equal_size_with_not_equal_start_index)
+TEST(Vector, can_assign_vectors_with_not_equal_size_with_not_equal_start_index)
 {
     Vector<int> vec1(3, 3);
     vec1[0] = 1;
@@ -171,8 +171,6 @@ TEST(Vector, can_assign_vectos_with_not_equal_size_with_not_equal_start_index)
 
     EXPECT_EQ(vec1, vec2);
 }
-
-
 
 TEST(Vector, can_add_vectors_with_equal_size_without_start_index)
 {
@@ -248,6 +246,7 @@ TEST(Vector, add_operator_applied_to_vectors_of_not_equal_size_with_equal_start_
     vec2[0] = 1;
     vec2[1] = 2;
     vec2[2] = 10;
+    vec2[3] = 10;
 
     ASSERT_ANY_THROW(vec1 + vec2);
 }
@@ -263,6 +262,7 @@ TEST(Vector, add_operator_applied_to_vectors_of_not_equal_size_with_not_equal_st
     vec2[0] = 1;
     vec2[1] = 2;
     vec2[2] = 10;
+    vec2[3] = 10;
 
     ASSERT_ANY_THROW(vec1 + vec2);
 }
@@ -318,7 +318,12 @@ TEST(Vector, can_add_vector_and_number_without_start_index)
 
     vec1 = vec1 + 100;
 
-    EXPECT_EQ(110, vec1[2]);
+    Vector<int> vec2(3);
+    vec2[0] = 101;
+    vec2[1] = 102;
+    vec2[2] = 110;
+
+    EXPECT_EQ(vec1, vec2);
 }
 
 TEST(Vector, can_add_vector_and_number_with_start_index)
@@ -330,7 +335,12 @@ TEST(Vector, can_add_vector_and_number_with_start_index)
 
     vec1 = vec1 + 100;
 
-    EXPECT_EQ(110, vec1[2]);
+    Vector<int> vec2(3, 3);
+    vec2[0] = 101;
+    vec2[1] = 102;
+    vec2[2] = 110;
+
+    EXPECT_EQ(vec1, vec2);
 }
 
 
@@ -343,7 +353,12 @@ TEST(Vector, can_subtract_number_from_vector_without_start_index)
 
     vec1 = vec1 - 100;
 
-    EXPECT_EQ(-90, vec1[2]);
+    Vector<int> vec2(3);
+    vec2[0] = -99;
+    vec2[1] = -98;
+    vec2[2] = -90;
+
+    EXPECT_EQ(vec1, vec2);
 }
 
 TEST(Vector, can_subtract_number_from_vector_with_start_index)
@@ -355,7 +370,12 @@ TEST(Vector, can_subtract_number_from_vector_with_start_index)
 
     vec1 = vec1 - 100;
 
-    EXPECT_EQ(-90, vec1[2]);
+    Vector<int> vec2(3, 3);
+    vec2[0] = -99;
+    vec2[1] = -98;
+    vec2[2] = -90;
+
+    EXPECT_EQ(vec1, vec2);
 }
 
 TEST(Vector, can_multiply_vector_and_number_without_start_index)
@@ -365,9 +385,14 @@ TEST(Vector, can_multiply_vector_and_number_without_start_index)
     vec1[1] = 2;
     vec1[2] = 10;
 
-    vec1 = vec1 * 100;
 
-    EXPECT_EQ(1000, vec1[2]);
+    vec1 = vec1 * 100;
+    Vector<int> vec2(3);
+    vec2[0] = 100;
+    vec2[1] = 200;
+    vec2[2] = 1000;
+
+    EXPECT_EQ(vec1, vec2);
 }
 
 TEST(Vector, can_multiply_vector_and_number_with_start_index)
@@ -378,11 +403,14 @@ TEST(Vector, can_multiply_vector_and_number_with_start_index)
     vec1[2] = 10;
 
     vec1 = vec1 * 100;
+ 
+    Vector<int> vec2(3, 3);
+    vec2[0] = 100;
+    vec2[1] = 200;
+    vec2[2] = 1000;
 
-    EXPECT_EQ(1000, vec1[2]);
+    EXPECT_EQ(vec1, vec2);
 }
-
-
 
 TEST(Vector, can_subtraction_vectors_with_equal_size_without_start_index)
 {
@@ -428,7 +456,7 @@ TEST(Vector, subtraction_operator_applied_to_vectors_of_equal_size_with_not_equa
     vec2[1] = 2;
     vec2[2] = 10;
 
-    ASSERT_ANY_THROW( vec1 - vec2);
+    ASSERT_ANY_THROW(vec1 - vec2);
 }
 
 TEST(Vector, subtraction_operator_applied_to_vectors_of_not_equal_size_without_start_index)
@@ -616,10 +644,6 @@ TEST(Vector, multiplication_operator_applied_to_vectors_of_not_equal_size_with_n
     int res;
     ASSERT_ANY_THROW(res = vec1 * vec2);
 }
-
-
-
-
 
 TEST(Vector, correct_work_smultiplication_operator_without_start_index)
 {
