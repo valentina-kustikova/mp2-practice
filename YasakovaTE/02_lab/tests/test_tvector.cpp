dc7d574f5946b85449dba17beaf233ada9e911f5
unknown <ITMM-230042@Q113.local>
2023-11-22 12:19:02 +0300

    fix

--- YasakovaTE/02_lab/tests/test_tvector.cpp
+++ YasakovaTE/02_lab/tests/test_tvector.cpp
@@ -7,6 +7,12 @@ TEST(TVector, can_create_vector_with_positive_length)
 	ASSERT_NO_THROW(TVector<int> v(5));
 }
 
+
+TEST(TVector, can_create_vector_with_positive_length_with_start_index)
+{
+	ASSERT_NO_THROW(TVector<int> v(5,1));
+}
+
 TEST(TVector, cant_create_too_large_vector)
 {
 	ASSERT_ANY_THROW(TVector<int> v(MAX_VECTOR_SIZE + 1));
@@ -29,6 +35,13 @@ TEST(TVector, can_create_copied_vector)
 	ASSERT_NO_THROW(TVector<int> v1(v));
 }
 
+TEST(TVector, can_create_copied_vector_start_index)
+{
+	TVector<int> v(10, 1);
+
+	ASSERT_NO_THROW(TVector<int> v1(v));
+}
+
 TEST(TVector, copied_vector_is_equal_to_source_one)
 {
 	TVector<int> t1(3);
@@ -37,9 +50,26 @@ TEST(TVector, copied_vector_is_equal_to_source_one)
 	EXPECT_EQ(t1, t2);
 }
 
+TEST(TVector, copied_vector_is_equal_to_source_one_start_index)
+{
+	TVector<int> t1(3, 0);
+	t1[0] = 3;  t1[1] = 3; t1[2] = 3;
+	TVector<int> t2(t1);
+	EXPECT_EQ(t1, t2);
+}
+
 TEST(TVector, copied_vector_has_its_own_memory)
 {
 	TVector<int> t1(3);
+	t1[0] = 3;  t1[1] = 3; t1[2] = 3;
+	TVector<int> t2(t1);
+	t1[0] = 2;
+	EXPECT_NE(t1, t2);
+}
+
+TEST(TVector, copied_vector_has_its_own_memory_start_index)
+{
+	TVector<int> t1(3,0);
 	t1[0] = 3;  t1[1] = 3; t1[2] = 3;
 	TVector<int> t2(t1);
 	t1[0] = 2;
@@ -68,9 +98,23 @@ TEST(TVector, can_set_and_get_element)
 	EXPECT_EQ(4, v[0]);
 }
 
+TEST(TVector, can_set_and_get_element_start_index)
+{
+	TVector<int> v(4, 0);
+	v[0] = 4;
+
+	EXPECT_EQ(4, v[0]);
+}
+
 TEST(TVector, throws_when_set_element_with_negative_index)
 {
 	TVector<int> t1(3);
+	ASSERT_ANY_THROW(t1[-1]);
+}
+
+TEST(TVector, throws_when_set_element_with_negative_index_start_index)
+{
+	TVector<int> t1(3,1);
 	ASSERT_ANY_THROW(t1[-1]);
 }
 
@@ -80,15 +124,37 @@ TEST(TVector, throws_when_set_element_with_too_large_index)
 	ASSERT_ANY_THROW(t1[100000000]);
 }
 
+TEST(TVector, throws_when_set_element_with_too_large_index_start_index)
+{
+	TVector<int> t1(3,1);
+	ASSERT_ANY_THROW(t1[100000000]);
+}
+
 TEST(TVector, can_assign_vector_to_itself)
 {
 	TVector<int> t1(3);
 	ASSERT_NO_THROW(t1 = t1);
 }
 
+TEST(TVector, can_assign_vector_to_itself_start_index)
+{
+	TVector<int> t1(3, 1);
+	ASSERT_NO_THROW(t1 = t1);
+}
+
 TEST(TVector, can_assign_vectors_of_equal_size)
 {
 	TVector<int> t1(3), t2(3);
+	t1[0] = 2;
+	t1[1] = 3;
+	t1[2] = 3;
+	t2 = t1;
+	EXPECT_EQ(t1, t2);
+}
+
+TEST(TVector, can_assign_vectors_of_equal_size_start_index)
+{
+	TVector<int> t1(3,0), t2(3,0);
 	t1[0] = 2;
 	t1[1] = 3;
 	t1[2] = 3;
@@ -105,9 +171,26 @@ TEST(TVector, assign_operator_diff_vector_size)
 	EXPECT_EQ(t1, t2);
 }
 
+TEST(TVector, assign_operator_diff_vector_size_start_index)
+{
+	TVector<int> t1(3,0), t2(2,0);
+	t1[0] = 2;
+	t1[1] = 3;
+	t2 = t1;
+	EXPECT_EQ(t1, t2);
+}
+
 TEST(TVector, can_assign_vectors_of_different_size)
 {
 	TVector<int> t1(3), t2(2);
+	t1[0] = 2;
+	t1[1] = 3;
+	ASSERT_NO_THROW(t2 = t1);
+}
+
+TEST(TVector, can_assign_vectors_of_different_size_start_index)
+{
+	TVector<int> t1(3,0), t2(2,0);
 	t1[0] = 2;
 	t1[1] = 3;
 	ASSERT_NO_THROW(t2 = t1);
@@ -123,9 +206,27 @@ TEST(TVector, compare_equal_vectors_return_true)
 	EXPECT_EQ(t1, t2);
 }
 
+TEST(TVector, compare_equal_vectors_return_true_start_index)
+{
+	TVector<int> t1(2,0), t2(2,0);
+	t1[0] = 2;
+	t1[1] = 3;
+	t2[0] = 2;
+	t2[1] = 3;
+	EXPECT_EQ(t1, t2);
+}
+
 TEST(TVector, compare_vector_with_itself_return_true)
 {
 	TVector<int> t1(2);
+	t1[0] = 2;
+	t1[1] = 3;
+	EXPECT_EQ(t1, t1);
+}
+
+TEST(TVector, compare_vector_with_itself_return_true_start_index)
+{
+	TVector<int> t1(2,0);
 	t1[0] = 2;
 	t1[1] = 3;
 	EXPECT_EQ(t1, t1);
@@ -141,9 +242,32 @@ TEST(TVector, vectors_with_different_size_are_not_equal)
 	EXPECT_NE(t1, t2);
 }
 
+TEST(TVector, vectors_with_different_size_are_not_equal_start_index)
+{
+	TVector<int> t1(3,0), t2(2,0);
+	t1[0] = 2;
+	t1[1] = 3;
+	t2[0] = 2;
+	t2[1] = 3;
+	EXPECT_NE(t1, t2);
+}
+
 TEST(TVector, can_add_scalar_to_vector)
 {
 	TVector<int> t1(3), t2(3);
+	t1[0] = 1;
+	t1[1] = 2;
+	t1[2] = 2;
+	t2[0] = 2;
+	t2[1] = 3;
+	t2[2] = 3;
+	t1 = t1 + 1;
+	EXPECT_EQ(t1, t2);
+}
+
+TEST(TVector, can_add_scalar_to_vector_start_index)
+{
+	TVector<int> t1(3,0), t2(3,0);
 	t1[0] = 1;
 	t1[1] = 2;
 	t1[2] = 2;
@@ -167,9 +291,35 @@ TEST(TVector, can_subtract_scalar_from_vector)
 	EXPECT_EQ(t1, t2);
 }
 
+TEST(TVector, can_subtract_scalar_from_vector_start_index)
+{
+	TVector<int> t1(3,0), t2(3,0);
+	t1[0] = 3;
+	t1[1] = 4;
+	t1[2] = 2;
+	t2[0] = 2;
+	t2[1] = 3;
+	t2[2] = 1;
+	t1 = t1 - 1;
+	EXPECT_EQ(t1, t2);
+}
+
 TEST(TVector, can_multiply_scalar_by_vector)
 {
 	TVector<int> t1(3), t2(3);
+	t1[0] = 2;
+	t1[1] = 3;
+	t1[2] = 1;
+	t2[0] = 4;
+	t2[1] = 6;
+	t2[2] = 2;
+	t1 = t1 * 2;
+	EXPECT_EQ(t1, t2);
+}
+
+TEST(TVector, can_multiply_scalar_by_vector_start_index)
+{
+	TVector<int> t1(3,0), t2(3,0);
 	t1[0] = 2;
 	t1[1] = 3;
 	t1[2] = 1;
@@ -193,9 +343,33 @@ TEST(TVector, can_add_vectors_with_equal_size)
 	EXPECT_EQ(t1, t2);
 }
 
+TEST(TVector, can_add_vectors_with_equal_size_start_index)
+{
+	TVector<int> t1(3,0), t2(3,0);
+	t1[0] = 2;
+	t1[1] = 3;
+	t1[2] = 1;
+	t2[0] = 4;
+	t2[1] = 6;
+	t2[2] = 2;
+	t1 = t1 + t1;
+	EXPECT_EQ(t1, t2);
+}
+
 TEST(TVector, cant_add_vectors_with_not_equal_size)
 {
 	TVector<int> t1(3), t2(2);
+	t1[0] = 2;
+	t1[1] = 3;
+	t1[2] = 1;
+	t2[0] = 4;
+	t2[1] = 6;
+	ASSERT_ANY_THROW(t1 + t2);
+}
+
+TEST(TVector, cant_add_vectors_with_not_equal_size_start_index)
+{
+	TVector<int> t1(3,0), t2(2,0);
 	t1[0] = 2;
 	t1[1] = 3;
 	t1[2] = 1;
@@ -218,9 +392,33 @@ TEST(TVector, can_subtract_vectors_with_equal_size)
 	EXPECT_EQ(t1, t2);
 }
 
+TEST(TVector, can_subtract_vectors_with_equal_size_start_index)
+{
+	TVector<int> t1(3,0), t2(3,0);
+	t1[0] = 2;
+	t1[1] = 3;
+	t1[2] = 1;
+	t2[0] = 0;
+	t2[1] = 0;
+	t2[2] = 0;
+	t1 = t1 - t1;
+	EXPECT_EQ(t1, t2);
+}
+
 TEST(TVector, cant_subtract_vectors_with_not_equal_size)
 {
 	TVector<int> t1(3), t2(2);
+	t1[0] = 2;
+	t1[1] = 3;
+	t1[2] = 1;
+	t2[0] = 4;
+	t2[1] = 6;
+	ASSERT_ANY_THROW(t1 - t2);
+}
+
+TEST(TVector, cant_subtract_vectors_with_not_equal_size_start_index)
+{
+	TVector<int> t1(3,0), t2(2,0);
 	t1[0] = 2;
 	t1[1] = 3;
 	t1[2] = 1;
@@ -239,9 +437,30 @@ TEST(TVector, can_multiply_vectors_with_equal_size)
 	EXPECT_EQ(ans, 14);
 }
 
+TEST(TVector, can_multiply_vectors_with_equal_size_start_index)
+{
+	TVector<int> t1(3,0);
+	t1[0] = 2;
+	t1[1] = 3;
+	t1[2] = 1;
+	int ans = t1 * t1;
+	EXPECT_EQ(ans, 14);
+}
+
 TEST(TVector, cant_multiply_vectors_with_not_equal_size)
 {
 	TVector<int> t1(3), t2(2);
+	t1[0] = 2;
+	t1[1] = 3;
+	t1[2] = 1;
+	t2[0] = 4;
+	t2[1] = 6;
+	ASSERT_ANY_THROW(t1 * t2);
+}
+
+TEST(TVector, cant_multiply_vectors_with_not_equal_size_start_index)
+{
+	TVector<int> t1(3,0), t2(2,0);
 	t1[0] = 2;
 	t1[1] = 3;
 	t1[2] = 1;
