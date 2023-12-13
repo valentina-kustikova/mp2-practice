72baec8c0d81c628c667646180a7e880c8387866
Shkrebko-Misha <michael.shkrebko@gmail.com>
2023-12-13 08:57:57 +0300

    Stack

---
+++ ShkrebkoMS/include/tstack.h
@@ -0,0 +1,85 @@
+	#ifndef _STACK_H
+#define _STACK_H
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T> class Stack {
+private:
+	int top;
+	int maxSize;
+	T* elems;
+public:
+	Stack(int size = 10);
+	Stack(const Stack<T>& s);
+	~Stack();
+	T Pop();
+	T Top();
+	void Push(const T e);
+	bool IsFull()const { return top == maxSize - 1; }
+	bool IsEmpty()const { return top == -1; }
+	int GetTop() { return top; }
+
+
+
+
+};
+
+template <typename T>
+Stack<T>::Stack(int size) {
+	if (size <= 0) {
+		throw "Negative or zero size!";
+	}
+	top = -1;
+	maxSize = size;
+	elems = new T[maxSize];
+}
+
+
+
+
+template <typename T>
+Stack<T>::Stack(const Stack <T>& s) {
+	top = s.top;
+	maxSize = s.maxSize;
+	elems = new T[maxSize];
+	for (int i = 0; i <= top; i++) {
+		elems[i] = s.elems[i];
+	}
+}
+
+template <typename T>
+Stack<T>::~Stack() {
+	delete[] elems;
+	maxSize = 0;
+	top = -1;
+}
+
+template <typename T>
+T Stack<T>::Top() {
+	if (IsEmpty()) {
+		throw "Stack is empty!";
+	return elems[top];
+
+}
+
+
+template <typename T>
+void Stack<T>::Push(const T e) {
+	if (IsFull()) {
+		throw "Stack is full!";
+	}
+	elems[++top] = e;
+}
+
+
+template <typename T>
+T Stack<T>::Pop() {
+	if (IsEmpty()) {
+		throw "Stack is empty!";
+	}
+	return elems[top--];
+}
+
+#endif
---
+++ ShkrebkoMS/samples/sample_tstack.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include "tstack.h"
+using namespace std;
+
+int main()
+{
+	
+	cout << "Enter lenght of string Stack: ";
+	int len;
+	cin >> len;
+
+	Stack<string> stack(len);
+
+	int answer;
+	string element;
+	do {
+
+		cout << "1: PUSH" << endl;
+		cout << "2: POP" << endl;
+		cout << "3: TOP" << endl;
+		cout << "4: IS FULL ?" << endl;
+		cout << "5: IS EMPTY ?" << endl;
+		cout << "6: EXIT" << endl;
+		cout << "Choose: ";
+
+		cin >> answer;
+		switch (answer) {
+		case 1:
+			cout << "Enter element you would like push : ";
+			cin >> element;
+			stack.Push(element);
+			break;
+		case 2:
+			stack.Pop();
+			break;
+		case 3:
+			element = stack.Top();
+			cout << element << endl;
+			break;
+		case 4:
+			answer = stack.IsFull();
+			cout << answer << endl;
+			break;
+		case 5:
+			answer = stack.IsEmpty();
+			cout << answer << endl;
+		default:
+			break;
+		}
+	} while (answer != 6);
+	
+
+
+	return 0;
+}
