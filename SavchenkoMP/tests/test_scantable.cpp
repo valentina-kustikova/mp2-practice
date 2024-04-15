5b98a530cf533cff5eb34f6006ff1549173a2f13
MaxikGuy <S_Maksssim@mail.ru>
2024-04-15 17:21:16 +0300

    new tests

--- SavchenkoMP/tests/test_scantable.cpp
+++ SavchenkoMP/tests/test_scantable.cpp
@@ -1,6 +1,9 @@
 #include <gtest.h>
 
 #include "scantable.h"
+#include <iostream>
+
+using namespace std;
 
 /*
 TEST(ScanTable, TEST_NAME) {
@@ -8,3 +11,109 @@ TEST(ScanTable, TEST_NAME) {
 }
 */
 
+// ParameterizedConstructorTest
+//TEST(ScanTable, ParameterizedConstructorTest) {
+//	int size = 3;
+//	ASSERT_NO_THROW(ScanTable<int, string> st(3)); // œŒ◊≈Ã” –”√¿≈“—ﬂ Õ¿ <,> ???
+//}
+
+TEST(ScanTable, ParameterizedConstructorTest_IsFull) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+	EXPECT_EQ(false, st.IsFull());
+}
+
+TEST(ScanTable, ParameterizedConstructorTest_IsEmpty) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+	EXPECT_EQ(true, st.IsEmpty());
+}
+
+TEST(ScanTable, ParameterizedConstructorTest_IsTabEnded) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+	EXPECT_EQ(false, st.IsTabEnded());
+}
+
+TEST(ScanTable, ParameterizedConstructorTest_Reset) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+	EXPECT_EQ(false, st.Reset());
+}
+
+TEST(ScanTable, ParameterizedConstructorTest_Next) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+	EXPECT_EQ(false, st.Next());
+}
+
+TEST(ScanTable, ParameterizedConstructorTest_GetDataCount) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+
+	EXPECT_EQ(0, st.GetDataCount());
+}
+
+TEST(ScanTable, ParameterizedConstructorTest_GetMaxSize) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+	EXPECT_EQ(size, st.GetMaxSize());
+}
+
+TEST(ScanTable, ParameterizedConstructorTest_GetKey_throw) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+	EXPECT_ANY_THROW(st.GetKey());
+	//EXPECT_EQ(NULL, st.GetKey());
+}
+
+TEST(ScanTable, ParameterizedConstructorTest_GetData_throw) {
+	int size = 3;
+	ScanTable<int, string> st(3);
+
+	EXPECT_ANY_THROW(st.GetData());
+	//EXPECT_EQ(nullptr, st.GetData());
+}
+
+// InsertTest
+TEST(ScanTable, InsertTest) {
+
+}
+
+// FindTest
+TEST(ScanTable, FindTest) {
+
+}
+
+// RemoveTest
+TEST(ScanTable, RemoveTest) {
+
+}
+
+// CopyConstructorTest
+TEST(ScanTable, CopyConstructorTest) {
+
+}
+
+// TypeCastCOnstructorTest
+TEST(ScanTable, TypeCastCOnstructorTest) {
+
+}
+
+// GetKeyTest
+TEST(ScanTable, GetKeyTest) {
+
+}
+
+// GetDataTest
+TEST(ScanTable, GetDataTest) {
+
+}
--- SavchenkoMP/tests/test_tabrecord.cpp
+++ SavchenkoMP/tests/test_tabrecord.cpp
@@ -34,7 +34,7 @@ TEST(TabRecord, CopyConstructorTest_Key) {
 	TabRecord<int, string> tr1(key, data);
 	TabRecord<int, string> tr2(tr1);
 	
-	EXPECT_EQ(tr1.get_key(), tr2.get_key());
+	EXPECT_EQ(tr1.GetKey(), tr2.GetKey());
 }
 
 TEST(TabRecord, CopyConstructorTest_DataPointer) {
@@ -43,7 +43,7 @@ TEST(TabRecord, CopyConstructorTest_DataPointer) {
 	TabRecord<int, string> tr1(key, data);
 	TabRecord<int, string> tr2(tr1);
 
-	EXPECT_EQ(tr1.get_data(), tr2.get_data());
+	EXPECT_EQ(tr1.GetData(), tr2.GetData());
 }
 
 TEST(TabRecord, CopyConstructorTest_Data) {
@@ -52,7 +52,7 @@ TEST(TabRecord, CopyConstructorTest_Data) {
 	TabRecord<int, string> tr1(key, data);
 	TabRecord<int, string> tr2(tr1);
 
-	EXPECT_EQ(*tr1.get_data(), *tr2.get_data());
+	EXPECT_EQ(*tr1.GetData(), *tr2.GetData());
 }
 
 // GetKeyTest
@@ -61,7 +61,7 @@ TEST(TabRecord, GetKeyTest) {
 	string* data = new string("a");
 	TabRecord<int, string> tr(key, data);
 
-	EXPECT_EQ(key, tr.get_key());
+	EXPECT_EQ(key, tr.GetKey());
 }
 
 // GetDataTest_Pointer
@@ -70,7 +70,7 @@ TEST(TabRecord, GetDataTest_Pointer) {
 	string* data = new string("a");
 	TabRecord<int, string> tr(key, data);
 
-	EXPECT_EQ(data, tr.get_data());
+	EXPECT_EQ(data, tr.GetData());
 }
 
 TEST(TabRecord, GetDataTest_Data) {
@@ -78,5 +78,5 @@ TEST(TabRecord, GetDataTest_Data) {
 	string* data = new string("a");
 	TabRecord<int, string> tr(key, data);
 
-	EXPECT_EQ("a", *tr.get_data());
+	EXPECT_EQ("a", *tr.GetData());
 }
