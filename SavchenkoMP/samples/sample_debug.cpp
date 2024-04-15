dad97cd6485a8f6efb27aaf811cb905d58680d22
MaxikGuy <S_Maksssim@mail.ru>
2024-04-15 17:22:17 +0300

    new debugging file

--- SavchenkoMP/samples/sample_debug.cpp
+++ SavchenkoMP/samples/sample_debug.cpp
@@ -6,45 +6,24 @@
 #include "tringlist.h"
 #include "tmonom.h"
 #include "tpolynom.h"
+#include "tableslib.h"
 
 using namespace std;
 
 int main() {
 	try {
-				
+		int size = 3;
+		ScanTable<int, string> st(3);
 
-		/*
-		TPolynom p1("3.3*x*y - 1.1*x^2");
-		TPolynom p2("5.5*x*y - 3.3*x^2");
-		TPolynom expP("-2.2*x*y + 2.2*x^2");
-
-		TPolynom p3(p1 - p2);
-		*/
-
-		TPolynom p1("2*x^2 + 3*x*y + 4*y^2");
-		TPolynom p2("3*x^3 + 2*x*y - 5*y^3");
-
-		//p1 + p2;
-		TPolynom exp_add = p1 + p2;
-		TPolynom exp_sub = p1 - p2;
-		TPolynom exp_mult = p1 * p2;
-
-		TPolynom p3("x^3*y*z + x*y^3*z + x*y*z^3 + x + y + z");
-		TPolynom exp_dX = p3.dif_x();
-		TPolynom exp_dY = p3.dif_y();
-		TPolynom exp_dZ = p3.dif_z();
-
-		cout << p1 << endl;
-		cout << p2 << endl << endl;
-
-		cout << exp_add << endl;
-		cout << exp_sub << endl;
-		cout << exp_mult << endl << endl;
-
-		cout << p3 << endl;
-		cout << exp_dX << endl;
-		cout << exp_dY << endl;
-		cout << exp_dZ << endl << endl;
+		/*cout << st.IsFull() << endl;
+		cout << st.IsEmpty() << endl;
+		cout << st.IsTabEnded() << endl;
+		cout << st.Reset() << endl;
+		cout << st.Next() << endl;
+		cout << st.GetDataCount() << endl;
+		cout << st.GetMaxSize() << endl;*/
+		//cout << st.GetKey() << endl;
+		//cout << st.GetData() << endl;
 	}
 	catch (string exp) {
 		cout << exp << endl;
