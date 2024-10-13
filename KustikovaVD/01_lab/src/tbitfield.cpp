78d21ee7b843f7b761bebbfe60d59a5bd3c973c0
Korobokkk <bbb754748@gmail.com>
2024-10-13 15:47:14 +0300

    started, constructor implemented

--- KustikovaVD/01_lab/src/tbitfield.cpp
+++ KustikovaVD/01_lab/src/tbitfield.cpp
@@ -13,6 +13,12 @@ static TBitField FAKE_BITFIELD(1);
 
 TBitField::TBitField(int len)
 {
+    BitLen = len;
+    MemLen = (len - 1) / (sizeof(TELEM) * 8) + 1;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++) {
+        pMem[i] = 0;
+    }
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
