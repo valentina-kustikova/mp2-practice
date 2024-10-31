0a75afe23f0cd3ff8f951a238c8310a37fcb5f26
unknown <ITMM-230041@Q113.local>
2024-10-31 14:53:05 +0300

    7th

--- VotincevDS/02_lab/test/test_tmatrix.cpp
+++ VotincevDS/02_lab/test/test_tmatrix.cpp
@@ -123,22 +123,52 @@ TEST(TDynamicMatrix, assign_operator_change_matrix_size)
 
 TEST(TDynamicMatrix, can_assign_matrices_of_different_size)
 {
-  ADD_FAILURE();
+    TDynamicMatrix<int> m(2);
+    TDynamicMatrix<int> m1(10);
+    m[0][0] = 1;
+    m[0][1] = 2;
+    m[1][0] = 3;
+    m[1][1] = 4;
+    ASSERT_NO_THROW(m1 = m);
 }
 
 TEST(TDynamicMatrix, compare_equal_matrices_return_true)
 {
-  ADD_FAILURE();
+    TDynamicMatrix<int> m(2);
+    TDynamicMatrix<int> m1(2);
+    m[0][0] = 1;
+    m[0][1] = 2;
+    m[1][0] = 3;
+    m[1][1] = 4;
+    m1[0][0] = 1;
+    m1[0][1] = 2;
+    m1[1][0] = 3;
+    m1[1][1] = 4;
+
+    EXPECT_EQ(1, m == m1);
 }
 
 TEST(TDynamicMatrix, compare_matrix_with_itself_return_true)
 {
-  ADD_FAILURE();
+    TDynamicMatrix<int> m(2);
+    m[0][0] = 1;
+    m[0][1] = 2;
+    m[1][0] = 3;
+    m[1][1] = 4;
+    EXPECT_EQ(1, m == m);
 }
 
 TEST(TDynamicMatrix, matrices_with_different_size_are_not_equal)
 {
-  ADD_FAILURE();
+    TDynamicMatrix<int> m(2);
+    TDynamicMatrix<int> m1(1);
+    m[0][0] = 1;
+    m[0][1] = 2;
+    m[1][0] = 3;
+    m[1][1] = 4;
+    m1[0][0] = 5;
+    EXPECT_EQ(0, m == m1);
+   
 }
 
 TEST(TDynamicMatrix, can_add_matrices_with_equal_size)
@@ -193,6 +223,15 @@ TEST(TDynamicMatrix, can_subtract_matrices_with_equal_size)
 
 TEST(TDynamicMatrix, cant_subtract_matrixes_with_not_equal_size)
 {
-  ADD_FAILURE();
+    TDynamicMatrix<int> m(2);
+    TDynamicMatrix<int> m1(1);
+    m[0][0] = 1;
+    m[0][1] = 2;
+    m[1][0] = 3;
+    m[1][1] = 4;
+    m1[0][0] = 5;
+
+    ASSERT_ANY_THROW(m - m1);
+
 }
 
