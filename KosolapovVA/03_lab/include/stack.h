3a225bce0f21c3e52b03e5127987c45cb8b7cc69
Kosolapov-Vitaliy <ITMM-230041@Q113.local>
2024-11-14 15:40:11 +0300

    Class update, make Abstract Class

--- KosolapovVA/03_lab/include/stack.h
+++ KosolapovVA/03_lab/include/stack.h
@@ -5,14 +5,64 @@
 template <typename T>
 class Stack
 {
-protected:
-    T* pMem;
-    int top;
 public:
-    Stack(int i);
-    ~Stack();
-    void Pop();
-    void Push(T el);
-    bool IsEmpty();
+    virtual void Pop()=0;
+    virtual void Push(const T&el)=0;
+    virtual T Top()=0;
+    virtual bool IsEmpty()=0;
+    virtual bool IsFull()=0;
 };
+
+template <typename T>
+class ArrStack :public Stack<T>
+{
+private:
+    int top;
+    size_t max_sz;
+    T* pMem;
+public:
+    ArrStack():top(-1), max_sz(0){}
+
+    ArrStack(int sz) :top(-1), max_sz(sz)
+    {
+        pMem = new T[sz];
+    }
+
+    ~ArrStack()
+    {
+        delete[] pMem;
+    }
+
+    void Pop()
+    {
+        if (IsEmpty())
+            throw std::exception("Stack is Empty");
+        top--;
+    }
+
+    void Push(const T& el)
+    {
+        if (IsFull())
+        {
+            max_sz = max_sz + 8;
+            T* tmp = pMem;
+            pMem = new T[max_sz];
+            std::copy(tmp, tmp + max_sz, pMem);
+        }
+        top++;
+        pMem[top] = el;
+    }
+
+    T Top()
+    {
+        if (IsEmpty())
+            throw std::exception("Stack is Empty");
+        return pMem[top];
+    }
+
+    bool IsEmpty() { return top == -1; }
+
+    bool IsFull() { return top == (max_sz - 1); }
+};
+
 #endif 
