e3c0691c17c5e0de13608fff3db886d3c0417ef9
ivan-sannikov <sanvan0094@gmail.com>
2024-11-28 12:06:04 +0300

    make stack

---
+++ SannikovIM/03_lab/include/postform.h
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+template <typename T>
+class TStack {
+private:
+	T* pMem;
+	int maxSize;
+	int top;
+public:
+	TStack(int maxSize = 10) {
+		this->maxSize = maxSize;
+		this->pMem = new T[maxSize];
+		for (int i = 0; i < maxSize; i++) {
+			this->pMem[i] = 0;
+		}
+		top = -1;
+	}
+	TStack(const TStack& ts) {
+		this->maxSize = ts.maxSize;
+		this->pMem = new T[this->maxSize];
+		for (int i = 0; i < this->maxSize; i++) {
+			this->pMem[i] = ts.pMem[i];
+		}
+		this->top = ts.top;
+	}
+	~TStack() {
+		delete[] pMem;
+	}
+
+	bool isFull() const {
+		return (this->top == this->maxSize-1);
+	}
+	bool isEmpty() const {
+		return (this->top ==  - 1)
+	}
+	void push(const T& val)
+	{
+		if (isFull) throw "error";
+		this->top++;
+		this->pMem[top] = val;
+	}
+	void pop() {
+		if (isEmpty) throw "error";
+		delete this->pMem[top];
+		this->top--;
+		
+	}
+
+	T Top() const {
+		return pMem[top];
+	}
+};
+
