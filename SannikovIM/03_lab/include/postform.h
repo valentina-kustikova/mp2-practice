7a5ae08ebecf65e5856faa7dc7950344d11adaec
ivan-sannikov <sanvan0094@gmail.com>
2024-12-05 12:13:01 +0300

    fix version v2

--- SannikovIM/03_lab/include/postform.h
+++ SannikovIM/03_lab/include/postform.h
@@ -31,17 +31,17 @@ public:
 		return (this->top == this->maxSize-1);
 	}
 	bool isEmpty() const {
-		return (this->top ==  - 1)
+		return (this->top == -1);
 	}
 	void push(const T& val)
 	{
-		if (isFull) throw "error";
+		if (isFull()) throw "error";
 		this->top++;
 		this->pMem[top] = val;
 	}
 	void pop() {
-		if (isEmpty) throw "error";
-		delete this->pMem[top];
+		if (isEmpty()) throw "error";
+
 		this->top--;
 		
 	}
@@ -49,5 +49,60 @@ public:
 	T Top() const {
 		return pMem[top];
 	}
+
+
 };
+char* getPostform(char* simpleForm, int n) {
+	TStack<char> s1(n);
+	TStack<char> s2(n);
+	for (int i = 0; i < n; i++) {
+		if (simpleForm[i] == '+' || simpleForm[i] == '-') {
+			if (s2.Top() == '*' || s2.Top() == '/') {
+				while (!s2.isEmpty()) {
+					s1.push(s2.Top());
+					s2.pop();
+				}
+				
+			}
+			s2.push(simpleForm[i]);
+		}
+		else if (simpleForm[i] == '*' || simpleForm[i] == '/') {
+				s2.push(simpleForm[i]);
+			
+		}
+		else if (simpleForm[i] == '(') s2.push(simpleForm[i]);
+		else if (simpleForm[i] == ')') {
+			while (!s2.isEmpty()) {
+				if (s2.Top() != '(') {
+					s1.push(s2.Top());
+					s2.pop();
+				}else{ s2.pop(); }
+			}
+		}
+		else {
+			s1.push(simpleForm[i]);
+		}
+	}
+	while (!s2.isEmpty()) {
+		s1.push(s2.Top());
+		s2.pop();
+	}
+	char* newForm = new char[n];
+	
+	while (!s2.isEmpty()) {
+		s1.push(s2.Top());
+		s2.pop();
+	}
+	int i = n-1;
+	while (!s1.isEmpty()) {
+		newForm[i] = s1.Top();
+		s1.pop();
+		i--;
+	}
+
+	return newForm;
+
+}
+
+
 
--- SannikovIM/03_lab/src/postform.cpp
+++ SannikovIM/03_lab/src/postform.cpp
@@ -1,1 +1,53 @@
 #include "postform.h"
+#include <string>
+#include <gtest.h>
+
+/*char* getPostform(char* simpleForm, int n) {
+	TStack<char> s1(n);
+	TStack<char> s2(n);
+	for (int i = 0; i < n; i++) {
+		if (simpleForm[i] == '+' || simpleForm[i] == '-') {
+			s2.push(simpleForm[i]);
+		}
+		else if (simpleForm[i] == '*' || simpleForm[i] == '/') {
+
+			if (s2.Top() == '+' || s2.Top() == '-') {
+				while (!s2.isEmpty()) {
+					s1.push(s2.Top());
+					s2.pop();
+				}
+				s2.push(simpleForm[i]);
+			}
+		}
+			//else if (simpleForm[i] == '(') s2.push(simpleForm[i]);
+			else if (simpleForm[i] == ')') {
+				while (!s2.isEmpty()) {
+					s1.push(s2.Top());
+					s2.pop();
+				}
+				s1.pop();
+			}
+			else {
+				s1.push(simpleForm[i]);
+			}
+	}
+	while (!s2.isEmpty()) {
+		s1.push(s2.Top());
+		s2.pop();
+	}
+	char* newForm = new char[n];
+	while (!s2.isEmpty()) {
+		s1.push(s2.Top());
+		s2.pop();
+	}
+	int i = 0;
+	while (!s1.isEmpty()) {
+		newForm[i] + s1.Top();
+		s1.pop();
+		i++;
+	}
+
+	return newForm;
+
+}*/
+
