53a342c4bc7c412f96ccfd4c0674f8974a9470ac
Korobokkk <bbb754748@gmail.com>
2024-12-05 07:53:18 +0300

    sample fix

--- KiselevIV/02_lab/samples/sample_matrix.cpp
+++ KiselevIV/02_lab/samples/sample_matrix.cpp
@@ -8,23 +8,25 @@
 #include "tmatrix.h"
 //---------------------------------------------------------------------------
 
+
 void main()
 {
-  TDynamicMatrix<int> a(5), b(5), c(5);
-  int i, j;
+    TDynamicMatrix<int> a(5), b(5), c(5);
+    int i, j;
 
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класса работы с матрицами"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+    setlocale(LC_ALL, "Russian");
+    cout << "Тестирование класса работы с матрицами"
+        << endl;
+    for (i = 0; i < 5; i++)
+        for (j = i; j < a[i].size(); j++)
+        {
+            a[i][j] = i;
+            b[i][j] = i + 13;
+        }
+    c = a + b;
+    cout << "Matrix a = " << endl << a << endl;
+    cout << "Matrix b = " << endl << b << endl;
+    cout << "Matrix c = a + b" << endl << c << endl;
+
 }
 //---------------------------------------------------------------------------
